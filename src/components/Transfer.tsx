41c5f7872cb87a65a5400c842fea708c4dce1ea8
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2023-01-24 14:05:03 -0500

    Add transfer component

---
+++ src/components/Transfer.tsx
@@ -0,0 +1,75 @@
+import { BigNumber, utils } from "ethers";
+import {
+  usePrepareSendTransaction,
+  useSendTransaction,
+  useContractWrite,
+  usePrepareContractWrite,
+  erc20ABI,
+  useContractRead,
+  UseContractConfig,
+} from "wagmi";
+
+type Props = {
+  token: string;
+  amount: string;
+  receiver: string;
+};
+
+export default function Transfer({ token, amount, receiver }: Props) {
+  const isEth =
+    token === ("ETH" || "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
+  console.log("is eth is", isEth);
+  // Send ETH
+  const { config } = usePrepareSendTransaction({
+    request: { to: receiver, value: BigNumber.from(amount) },
+  });
+  const { data, isLoading, isSuccess, sendTransaction } =
+    useSendTransaction(config);
+
+  // Send ERC20
+  const { config: tokenConfig, error } = usePrepareContractWrite({
+    address: `0x${token.replace(/^0x/, "")}`,
+    abi: erc20ABI,
+    functionName: "transfer",
+    args: [`0x${receiver.replace(/^0x/, "")}`, BigNumber.from(amount)],
+  });
+
+  const {
+    data: tokenData,
+    isLoading: tokenIsLoading,
+    isSuccess: tokenIsSuccess,
+    write: tokenWrite,
+  } = useContractWrite(tokenConfig);
+
+  // Get token symbol
+  const { data: tokenSymbol } = useContractRead({
+    address: `0x${token.replace(/^0x/, "")}`,
+    abi: erc20ABI,
+    functionName: "symbol",
+  });
+
+  return (
+    <div>
+      {isEth && (
+        <button
+          disabled={!sendTransaction}
+          onClick={() => sendTransaction?.()}
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight  rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          Send {utils.formatEther(amount)} ETH to{" "}
+          {receiver.slice(0, 4) + "..." + receiver.slice(-4)}
+        </button>
+      )}
+      {!isEth && (
+        <button
+          disabled={!tokenWrite}
+          onClick={() => tokenWrite?.()}
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight  rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          Send {utils.formatEther(amount)} {tokenSymbol} to{" "}
+          {receiver.slice(0, 4) + "..." + receiver.slice(-4)}
+        </button>
+      )}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Transfer from "@/components/Transfer";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { ChatRoom } from "src/pages/ChatRoom";
 
 export default function Home() {
@@ -5,6 +7,19 @@ export default function Home() {
     <>
       <div className="h-screen bg-gray-900 pt-32">
         <ChatRoom />
+        <div className="mt-10 flex justify-center items-center gap-4">
+          <ConnectButton />
+          <Transfer
+            token="0x187C0F98FEF80E87880Db50241D40551eDd027Bf"
+            amount="1000000000000000000"
+            receiver="0x637C1Ec1d205a4E7a79c9CE4Bd100CD1d19E6080"
+          />
+          <Transfer
+            token="ETH"
+            amount="1000000000000000000"
+            receiver="0x637C1Ec1d205a4E7a79c9CE4Bd100CD1d19E6080"
+          />
+        </div>
       </div>
     </>
   );
