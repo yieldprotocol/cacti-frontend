38d4aece779ccaef920fe8f79f4bacee42762f8b
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2023-01-25 15:21:16 -0500

    Move connect button to top right

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,14 @@ import { ChatRoom } from "src/pages/ChatRoom";
 export const Home = () => {
   return (
     <>
-      <div className="h-screen bg-gray-900 pt-32">
-        <ChatRoom />
-        <div className="mt-10 flex items-center justify-center gap-4">
+      <div className="h-screen bg-gray-900 ">
+        <div className="flex justify-end pt-2 pr-2">
           <ConnectButton />
+        </div>
+        <div className="pt-32">
+          <ChatRoom />
+        </div>
+        <div className="mt-10 flex items-center justify-center gap-4">
           <TransferButton
             token="0x187C0F98FEF80E87880Db50241D40551eDd027Bf"
             amount="1000000000000000000"
