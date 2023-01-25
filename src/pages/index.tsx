b5d41d155442990dba88f9b8e0bc5cf2edd268f7
wildmolasses <changes@gmail.com>
2023-01-25 13:20:30 -0500

    Small refactor
    Co-authored-by: Gary Ghayrat <61768337+garyghayrat@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import { TransferEth, TransferToken } from "@/components/Transfer";
+import { TransferButton } from "@/components/TransferButton";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { ChatRoom } from "src/pages/ChatRoom";
 
-export default function Home() {
+export const Home = () => {
   return (
     <>
       <div className="h-screen bg-gray-900 pt-32">
         <ChatRoom />
-        <div className="mt-10 flex justify-center items-center gap-4">
+        <div className="mt-10 flex items-center justify-center gap-4">
           <ConnectButton />
-          <TransferToken
+          <TransferButton
             token="0x187C0F98FEF80E87880Db50241D40551eDd027Bf"
             amount="1000000000000000000"
             receiver="0x637C1Ec1d205a4E7a79c9CE4Bd100CD1d19E6080"
           />
 
-          <TransferEth
+          <TransferButton
             amount="1000000000000000000"
             receiver="0x637C1Ec1d205a4E7a79c9CE4Bd100CD1d19E6080"
           />
@@ -23,4 +23,6 @@ export default function Home() {
       </div>
     </>
   );
-}
+};
+
+export default Home;
