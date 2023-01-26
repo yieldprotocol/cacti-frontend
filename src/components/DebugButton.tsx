c604989f97bab32f960c77f187142afe0669c81a
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2023-01-26 17:01:15 -0500

    Add flex

--- src/components/DebugButton.tsx
+++ src/components/DebugButton.tsx
@@ -9,9 +9,9 @@ export const DebugButton = () => {
     <div>
       <button
         onClick={() => setOpenModal(true)}
-        className="align-center inline-block font-bold rounded-xl bg-gray-700 px-6 py-2.5 text-md leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-gray-800 hover:shadow-lg focus:bg-gray-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg"
+        className="flex align-center font-bold rounded-xl bg-gray-700 px-6 py-2.5 text-md leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-gray-800 hover:shadow-lg focus:bg-gray-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg"
       >
-        Debug <ArrowTopRightOnSquareIcon className="h-5 w-5 inline" />
+        Debug <ArrowTopRightOnSquareIcon className="h-5 w-5 ml-1" />
       </button>
       <Modal openState={openModal} handleClose={() => setOpenModal(false)} />
     </div>
