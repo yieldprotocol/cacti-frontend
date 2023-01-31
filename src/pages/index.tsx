d84d626b6133f4a92b62a254befd6872d669369c
wildmolasses <changes@gmail.com>
2023-01-31 10:11:45 -0500

    small style stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { DebugButton } from '@/components/DebugButton';
 export const Home = () => {
   return (
     <>
-      <div className="flex h-screen bg-gray-800">
+      <div className="flex h-screen bg-gray-700">
         <div className="fixed top-0 right-0 mr-4 mt-4 inline-flex gap-3">
           <DebugButton />
           <ConnectButton />
