4a14164767904522b10f62e53b500687af5e086b
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2023-02-07 16:19:56 -0500

    add tokenList

---
+++ src/hooks/useTokenList.tsx
@@ -0,0 +1,19 @@
+import { useNetwork } from 'wagmi';
+import tokenListJson from '@/utils/TokenList.json';
+
+export interface TokenListItem {
+  chainId: number;
+  symbol: string;
+  address: string;
+  decimals: number;
+  name: string;
+  logoURI: string;
+}
+
+export const useTokenList = () => {
+  const { chain } = useNetwork();
+  const chainId = chain?.id || 1; // default to mainnet if no chain id
+  const tokenList = tokenListJson.tokens.filter((token) => token.chainId === chainId);
+  const baseToken = chain.nativeCurrency;
+  return { tokenList: [baseToken, ...tokenList] };
+};
