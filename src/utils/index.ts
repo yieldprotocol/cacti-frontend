a1d87d8ba026b401ca2be1fee0e24157b09c0ced
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2023-02-07 16:47:35 -0500

    Remove unused var

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { BigNumber, utils } from 'ethers';
+import { utils } from 'ethers';
 
 export const shortenAddress = (address: string) => address.slice(0, 6) + '...' + address.slice(-4);
 export const formatToEther = (amount: string) => utils.formatEther(amount);
