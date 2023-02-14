10178fc7e876facb440519dea0237548b4e41171
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2023-02-13 22:18:09 -0500

    Configure tenderly fork as provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,38 @@ import type { AppProps } from 'next/app';
 import { CenterProvider } from '@center-inc/react';
 import { RainbowKitProvider, getDefaultWallets, lightTheme } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
-import { WagmiConfig, configureChains, createClient, goerli } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import { Chain, WagmiConfig, configureChains, createClient } from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { ChatContextProvider } from '@/contexts/ChatContext';
 import { ModalContextProvider } from '@/contexts/ModalContext';
 import '@/styles/globals.css';
 
+const mainnetForkURL = process.env.MAINNET_FORK_URL;
+const mainnetFork = {
+  id: 1,
+  name: 'Mainnet Fork',
+  network: 'mainnetFork',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: { http: [mainnetForkURL] },
+    default: { http: [mainnetForkURL] },
+  },
+} as Chain;
+
 const { chains, provider } = configureChains(
-  [goerli],
-  [publicProvider(), alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY })]
+  [mainnetFork],
+  [
+    jsonRpcProvider({
+      priority: 0,
+      rpc: (chain) => ({
+        http: process.env.NEXT_PUBLIC_MAINNET_FORK_URL,
+      }),
+    }),
+  ]
 );
 
 const { connectors } = getDefaultWallets({
