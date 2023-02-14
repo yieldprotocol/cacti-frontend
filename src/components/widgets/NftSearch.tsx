75f92a43c8ca611a250ae9ad6e9f3e440027c2f3
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2023-02-14 14:46:38 -0500

    Add center.app link and columns

--- src/components/widgets/NftSearch.tsx
+++ src/components/widgets/NftSearch.tsx
@@ -1,48 +1,44 @@
 import { useCollectionSearch } from '@center-inc/react';
 
+interface Result {
+  id: any;
+  address: any;
+  relevance: any;
+  name: any;
+  previewImageUrl: any;
+}
+
 export const NftSearch = ({ query }) => {
   const { results, loading, error } = useCollectionSearch({
     query,
   });
   return (
     <>
-      <div className="text-black">
+      <div className="columns-1 text-black sm:columns-2">
         {loading && <span>Results loading</span>}
         <ul role="list" className="divide-y divide-gray-200">
-          {results?.map(
-            ({
-              id,
-              address,
-              relevance,
-              name,
-              previewImageUrl,
-            }: {
-              id: any;
-              address: any;
-              relevance: any;
-              name: any;
-              previewImageUrl: any;
-            }) => (
-              <li key={id}>
-                <a
-                  href={`https://etherscan.io/token/${address}`}
-                  className="flex py-4"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {previewImageUrl ? (
-                    <img className="h-10 w-10 rounded-full" src={previewImageUrl} alt="" />
-                  ) : (
-                    <div className="h-10 w-10"></div>
-                  )}
-                  <div className="ml-3">
-                    <p className="text-sm font-medium text-gray-900">{name}</p>
-                    <p className="text-sm text-gray-500">{address}</p>
+          {results?.map(({ id, address, relevance, name, previewImageUrl }: Result) => (
+            <div key={id}>
+              <a
+                href={`https://center.app/collections/${address}`}
+                className="flex py-4"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {previewImageUrl ? (
+                  <img className="h-40 w-40 rounded-md" src={previewImageUrl} alt="" />
+                ) : (
+                  <div className="h-40 w-40">
+                    <img
+                      src="https://image.freepik.com/free-icon/question-square_318-28245.jpg"
+                      alt="question mark"
+                    />
                   </div>
-                </a>
-              </li>
-            )
-          ) || ''}
+                )}
+                <p className="ml-3 text-sm font-medium text-gray-900">{name}</p>
+              </a>
+            </div>
+          )) || ''}
         </ul>
         {error && 'There was an unexpected center.app API error'}
       </div>
