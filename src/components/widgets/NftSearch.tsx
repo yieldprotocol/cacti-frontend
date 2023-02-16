6c654e5c9c6e93866aa3944669114a9be8406197
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2023-02-16 12:34:25 -0500

    Add link color and question mark

--- src/components/widgets/NftSearch.tsx
+++ src/components/widgets/NftSearch.tsx
@@ -21,21 +21,18 @@ export const NftSearch = ({ query }) => {
             <div key={id}>
               <a
                 href={`https://center.app/collections/${address}`}
-                className="flex py-4"
+                className="flex items-center py-4"
                 target="_blank"
                 rel="noreferrer"
               >
                 {previewImageUrl ? (
-                  <img className="h-40 w-40 rounded-md" src={previewImageUrl} alt="" />
+                  <img className="h-32 w-32 rounded-md" src={previewImageUrl} alt="" />
                 ) : (
-                  <div className="h-40 w-40">
-                    <img
-                      src="https://image.freepik.com/free-icon/question-square_318-28245.jpg"
-                      alt="question mark"
-                    />
+                  <div className="flex h-32 w-32 items-center justify-center bg-gray-100 text-4xl text-gray-400">
+                    ?
                   </div>
                 )}
-                <p className="ml-3 text-sm font-medium text-gray-900">{name}</p>
+                <p className="ml-3 text-sm font-medium text-blue-400 underline">{name}</p>
               </a>
             </div>
           )) || ''}
