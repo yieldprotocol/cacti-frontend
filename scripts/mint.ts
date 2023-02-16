5ca2452ff27b2341e80f942d0a190c18d0062682
wildmolasses <changes@gmail.com>
2023-02-16 14:01:23 -0600

    parameterizable mint

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -4,13 +4,16 @@ import { ethers } from 'ethers';
 config();
 
 const forkId = process.env.NEXT_PUBLIC_TENDERLY_FORK_ID;
+const [, , address] = process.argv;
+if (address === undefined || address.substring(0, 2) !== '0x')
+  throw new Error('Usage: yarn mint <address>');
 const forkRPC = `https://rpc.tenderly.co/fork/${forkId}`;
 
 const provider = new ethers.providers.JsonRpcProvider(forkRPC);
 const signer = provider.getSigner();
 
 const params = [
-  ['0x637C1Ec1d205a4E7a79c9CE4Bd100CD1d19E6080'],
+  [address],
   ethers.utils.parseEther('10').toHexString(), // hex encoded wei amount
 ];
 
