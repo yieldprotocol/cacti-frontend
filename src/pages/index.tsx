654aad5ad2b789fb5415102be7f070b89ced63da
Kah Keng Tay <kahkeng@gmail.com>
2023-02-24 17:18:03 -0800

    Add reset button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { ChatBox } from '@/components/ChatBox';
 import { DebugButton } from '@/components/DebugButton';
 import { MintButton } from '@/components/MintButton';
+import { ResetButton } from '@/components/ResetButton';
 
 export const Home = () => {
   return (
     <>
       <div className="flex h-screen bg-gray-700">
         <div className="fixed top-0 right-0 mr-4 mt-4 inline-flex gap-3">
+          <ResetButton />
           <MintButton />
           <DebugButton />
           <ConnectButton />
