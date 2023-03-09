024a887136116e654bcda21aeccf6843af8a814e
Keating <keating.dev@protonmail.com>
2023-03-09 09:50:39 -0500

    Support medium screens better

--- src/components/WelcomeMessage.tsx
+++ src/components/WelcomeMessage.tsx
@@ -10,7 +10,7 @@ const WelcomeBox = ({ onClick, children }: { onClick?: () => void; children: Rea
   const onClickClasses = onClick ? 'cursor-pointer hover:bg-gray-500' : '';
   return (
     <div
-      className={`my-4 max-w-[250px]  rounded-lg bg-gray-600 p-4 text-center text-white ${onClickClasses}`}
+      className={`my-4 max-w-[250px]  rounded-lg bg-gray-600 p-4 text-center text-white md:p-2 ${onClickClasses}`}
       onClick={onClick}
     >
       <p className="flex min-h-[48px] items-center justify-center">{children}</p>
@@ -25,8 +25,10 @@ const WelcomeBoxSubtitle = ({ children }) => {
 const WelcomeMessage = () => {
   const { sendMessage } = useChatContext();
   return (
-    <div className="mt-4 md:px-6">
-      <h1 className="mb-8 py-5 text-center text-4xl font-bold text-white">ChatWeb3</h1>
+    <div className="mt-4 md:mt-2 md:px-6">
+      <h1 className="md:pd-0 mb-8 py-5 text-center text-4xl font-bold text-white md:mb-2 md:pt-0 md:pb-2">
+        ChatWeb3
+      </h1>
       <div className="flex h-full w-full justify-center gap-x-8">
         <WelcomeColumn>
           <BoltIcon className="mt-3 h-12 text-gray-300" />
