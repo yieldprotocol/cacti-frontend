f143e6db23ccd4ae205abdf6c84d9ee0bd30b720
brucedonovan <bruce.donovan@gmail.com>
2023-03-16 17:15:50 +0000

    wrap all dev buttons into modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,14 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { ChatBox } from '@/components/ChatBox';
-import { DebugButton } from '@/components/DebugButton';
-import { DebugMessageToggle } from '@/components/DebugMessageToggle';
-import { MintButton } from '@/components/MintButton';
-import { ResetButton } from '@/components/ResetButton';
+import { DevToolsButton } from '@/components/devTools/DevToolsButton';
 
 export const Home = () => {
   return (
     <>
       <div className="flex h-screen bg-gray-700">
         <div className="fixed top-0 right-0 mr-4 mt-4 inline-flex gap-3">
-          <DebugMessageToggle />
-          <ResetButton />
-          <MintButton />
-          <DebugButton />
           <ConnectButton />
+          { true && <DevToolsButton /> /* TODO : add logic to show devTools: possible eg. process.env.NODE_ENV === 'development' */ } 
         </div>
         <div className="w-full">
           <ChatBox />
