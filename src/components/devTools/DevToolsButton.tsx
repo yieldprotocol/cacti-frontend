c63de2cd51035f9d467433e347abd0b92a5b9e3a
brucedonovan <bruce.donovan@gmail.com>
2023-03-17 07:51:07 +0000

    devTools button styling

--- src/components/devTools/DevToolsButton.tsx
+++ src/components/devTools/DevToolsButton.tsx
@@ -9,7 +9,7 @@ export const DevToolsButton = () => {
     <div>
       <button
         onClick={() => setOpenModal(true)}
-        className="align-center text-md flex rounded-xl bg-black px-6 py-2.5 font-bold leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-gray-800 hover:shadow-lg focus:shadow-lg active:shadow-lg"
+        className="align-center text-md flex rounded-xl border px-6 py-2.5 font-bold leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-gray-800 hover:shadow-lg focus:shadow-lg active:shadow-lg"
       >
         DevTools <ArrowTopRightOnSquareIcon className="ml-1 h-5 w-5" />
       </button>
