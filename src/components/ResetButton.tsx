c2453b6ca48f8c1ec8f15a08e4161d762b16da1f
brucedonovan <bruce.donovan@gmail.com>
2023-03-21 14:56:51 +0000

    ResetButton with named styles

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -2,7 +2,12 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import { Button } from './Button';
 
 type ResetButtonProps = {
-  styleOption?: number;
+  styleOption?: 'button' | 'iconAndText';
+};
+
+type ButtonStyles = {
+  button: JSX.Element;
+  iconAndText: JSX.Element;
 };
 
 export const ResetButton = (props: ResetButtonProps) => {
@@ -14,16 +19,17 @@ export const ResetButton = (props: ResetButtonProps) => {
     window.location.assign(paramString ? `/?${paramString}` : '/');
   };
 
-  const buttonStyles = [];
-  buttonStyles[0] = <Button onClick={reset}>Reset</Button>;
-  buttonStyles[1] = (
-    <button className="text-xs text-gray-500 hover:text-white" onClick={reset}>
-      <div className="p-1">
-        <ArrowPathIcon />
-      </div>
-      <div className="-m-1">Reset </div>
-    </button>
-  );
+  const buttonStyles: ButtonStyles = {
+    button: <Button onClick={reset}>Reset</Button>,
+    iconAndText: (
+      <button className="text-xs text-gray-500 hover:text-white" onClick={reset}>
+        <div className="p-1">
+          <ArrowPathIcon />
+        </div>
+        <div className="-m-1">Reset </div>
+      </button>
+    ),
+  };
 
-  return buttonStyles[props.styleOption || 0];
+  return buttonStyles[props.styleOption || 'button'];
 };
