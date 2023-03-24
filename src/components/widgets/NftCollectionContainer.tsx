e87c276886e8e8afee4a5cc16caa08726a0d3091
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-24 09:58:04 -0700

    feat: carousel for nft collections

--- src/components/widgets/NftCollectionContainer.tsx
+++ src/components/widgets/NftCollectionContainer.tsx
@@ -45,20 +45,26 @@ export const NftCollectionContainer = ({
   previewImageUrl,
 }: NftCollectionContainerProps) => {
   return (
-    <>
+    <li>
       <a
         href={`https://center.app/${network}/collections/${address}`}
-        className="flex flex-col items-center justify-center py-4"
+        className="flex flex-col items-center justify-center"
         target="_blank"
         rel="noreferrer"
       >
-        <img className="h-32 w-32 rounded-md" src={previewImageUrl} alt="" />
+        <div className="rounded-xl bg-gray-800 shadow-sm">
+          <img
+            className="h-[160px] min-w-[160px] rounded-xl hover:opacity-60"
+            src={previewImageUrl}
+            alt=""
+          />
+        </div>
         <div className="mt-1 flex flex-col items-center justify-center">
-          <p className="ml-3 text-xs font-medium text-blue-400 underline">{name}</p>
+          <p className="ml-3 text-xs font-bold text-gray-200 hover:underline">{name}</p>
           <p className="ml-3 text-xs font-medium text-gray-400">{network}</p>
         </div>
       </a>
-    </>
+    </li>
   );
 };
 
--- src/components/widgets/NftSearch.tsx
+++ src/components/widgets/NftSearch.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef, useState } from 'react';
 import { useCollectionSearch } from '@center-inc/react';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/20/solid';
 import { ETHEREUM_NETWORK } from '@/utils/constants';
 import { NftCollectionContainer } from './NftCollectionContainer';
 
@@ -10,17 +12,83 @@ interface Result {
   previewImageUrl: any;
 }
 
+interface ScrollButtonProps {
+  direction: 'left' | 'right';
+  onClick: () => void;
+  styleProps?: string;
+  disabled?: boolean;
+}
+
+const ScrollButton = ({ direction, onClick, styleProps, disabled }: ScrollButtonProps) => {
+  return (
+    <button
+      className={`${styleProps} h-8 w-8 rounded-full bg-gray-700/50 p-1 text-gray-200 hover:bg-gray-800/50 disabled:bg-gray-700/20`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {direction === 'left' ? <ArrowLeftIcon /> : <ArrowRightIcon />}
+    </button>
+  );
+};
+
 export const NftSearch = ({ query }) => {
   const { results, loading, error } = useCollectionSearch({
     query,
   });
+
+  const [arrowLeftDisabled, setArrowLeftDisabled] = useState(false);
+  const [arrowRightDisabled, setArrowRightDisabled] = useState(false);
+  const [hasScoll, setHasScroll] = useState(false);
+
+  const slider = useRef<HTMLUListElement>();
+  const sliderWrap = useRef<HTMLDivElement>();
+
+  const handleScroll = (direction: 'left' | 'right') => {
+    if (direction === 'left') {
+      slider.current.scrollLeft -= 200;
+    } else {
+      slider.current.scrollLeft += 200;
+    }
+
+    slider.current.scrollLeft <= 0 ? setArrowLeftDisabled(true) : setArrowLeftDisabled(false);
+    slider.current.scrollLeft >= slider.current.scrollWidth - slider.current.offsetWidth
+      ? setArrowRightDisabled(true)
+      : setArrowRightDisabled(false);
+  };
+
+  useEffect(() => {
+    // check if the slider has scroll
+    setTimeout(() => {
+      if (slider.current.clientWidth < slider.current.scrollWidth) {
+        setHasScroll(true);
+      }
+    }, 100); // wait for the slider to render
+  }, [slider]);
+
   return (
-    <>
-      <div className="mt-4 flex w-[100%] justify-center text-black">
+    <div className="mt-4 w-[100%]">
+      {hasScoll && (
+        <div className="flex justify-start gap-2">
+          <ScrollButton
+            direction="left"
+            onClick={() => handleScroll('left')}
+            styleProps={arrowLeftDisabled ? 'disabled disabled:hover:cursor-not-allowed' : ''}
+            disabled={arrowLeftDisabled}
+          />
+          <ScrollButton
+            direction="right"
+            onClick={() => handleScroll('right')}
+            styleProps={arrowRightDisabled ? 'disabled disabled:hover:cursor-not-allowed' : ''}
+            disabled={arrowRightDisabled}
+          />
+        </div>
+      )}
+      <div className="mt-4 flex w-[100%] justify-center text-black" ref={sliderWrap}>
         {loading && <span>Results loading</span>}
         <ul
           role="list"
-          className="flex grid w-[100%] grid-cols-3 flex-wrap gap-x-6 gap-y-1 after:flex-auto xl:grid-cols-4"
+          className="no-scrollbar flex w-[100%] gap-4 overflow-x-auto scroll-smooth"
+          ref={slider}
         >
           {results?.map(({ id, ...props }: Result) => {
             if (props.previewImageUrl) {
@@ -30,6 +98,6 @@ export const NftSearch = ({ query }) => {
         </ul>
         {error && 'There was an unexpected center.app API error'}
       </div>
-    </>
+    </div>
   );
 };
