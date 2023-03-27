a8ca5bc0b34f04e211a4c7cbf645eb683aac01ea
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-27 14:23:37 -0700

    feat: use local storage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+const getStorageValue = (key: string, defaultValue: string) => {
+  const itemInLocalStorage: string | boolean | null = JSON.parse(localStorage.getItem(key)!);
+  return itemInLocalStorage ?? defaultValue;
+};
+
+export const useLocalStorage = (key: string, defaultValue: string) => {
+  const [value, setValue] = useState<string | boolean>(() => getStorageValue(key, defaultValue));
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+};
