f4e795c68d0a94f81819d39c0e76611aa8259ce4
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-27 14:24:28 -0700

    chore: remove unused

--- src/components/ApproveTokens.tsx
+++ src/components/ApproveTokens.tsx
@@ -1,15 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { BigNumber } from 'ethers';
-import { useNetwork } from 'wagmi';
 import { Button } from '@/components/Button';
 import useTokenApproval from '@/hooks/useTokenApproval';
 import { Token } from '@/types';
-
-interface Props {
-  tokenIn: Token;
-  tokenOut: Token;
-  amountIn: BigNumber;
-}
 
 const ApproveTokens = ({
   token,
@@ -22,7 +15,6 @@ const ApproveTokens = ({
   spenderAddress: `0x${string}`;
   setIsApprovalSuccess: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-  const { chain } = useNetwork();
   // Get approval ready
   const { approvalWrite, isLoading, isSuccess, data } = useTokenApproval({
     address: token.address as `0x${string}`,
