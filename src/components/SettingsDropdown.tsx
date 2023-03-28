addc7a5634b26dfe4adabb8ee31c559ab8f0452f
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-28 11:22:41 -0700

    feat: use fork toggle

--- src/components/SettingsDropdown.tsx
+++ src/components/SettingsDropdown.tsx
@@ -1,9 +1,12 @@
 import { Fragment, useState } from 'react';
-import { Menu, Transition } from '@headlessui/react';
+import { Menu, Switch, Transition } from '@headlessui/react';
+import { SignalIcon } from '@heroicons/react/20/solid';
 import { Cog8ToothIcon, WrenchIcon } from '@heroicons/react/24/outline';
 import { DevToolsModal } from '@/components/devTools/DevToolsModal';
+import useFork from '@/hooks/useFork';
 
 const SettingsDropdown = () => {
+  const { useForkEnv, setUseForkEnv } = useFork();
   const [openModal, setOpenModal] = useState(false);
   return (
     <Menu as="div" className="relative">
@@ -19,7 +22,36 @@ const SettingsDropdown = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 mt-4 w-56 origin-top-right rounded-md bg-gray-800 shadow-md focus:outline-none">
+        <Menu.Items
+          className="absolute right-0 mt-4 w-56 origin-top-right rounded-md bg-gray-800 shadow-md focus:outline-none"
+          static
+        >
+          <Menu.Item>
+            <div className="p-2">
+              <button
+                className="text-md flex w-full items-center gap-2 rounded-md p-2 px-2 py-2 text-white hover:bg-gray-700"
+                onClick={() => setUseForkEnv(!useForkEnv)}
+              >
+                <SignalIcon className="ml-1 h-5 w-5 text-white" />
+                <div className="flex w-full justify-between">
+                  <div>Use Fork</div>
+                  <Switch
+                    checked={useForkEnv}
+                    onChange={setUseForkEnv}
+                    className={`${
+                      useForkEnv ? 'bg-gray-600' : 'bg-gray-400'
+                    } relative inline-flex h-6 w-11 items-center rounded-full`}
+                  >
+                    <span
+                      className={`${
+                        useForkEnv ? 'translate-x-6' : 'translate-x-1'
+                      } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                    />
+                  </Switch>
+                </div>
+              </button>
+            </div>
+          </Menu.Item>
           <Menu.Item>
             <div className="p-2">
               <button
