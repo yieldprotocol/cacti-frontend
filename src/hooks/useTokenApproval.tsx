e7499082a397bf3d36e8b6d195a799362001ff09
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-29 08:28:14 -0700

    feat: handle fork

--- src/hooks/useTokenApproval.tsx
+++ src/hooks/useTokenApproval.tsx
@@ -1,33 +1,76 @@
+import { useState } from 'react';
 import { BigNumber } from 'ethers';
 import {
   erc20ABI,
   useAccount,
+  useContract,
   useContractRead,
   useContractWrite,
-  useNetwork,
   usePrepareContractWrite,
   useWaitForTransaction,
 } from 'wagmi';
-import { UNISWAP_ROUTER_02_ADDRESS } from '@/utils/constants';
+import useFork from './useFork';
+import useSigner from './useSigner';
 
 const useTokenApproval = ({
   address,
-  amountIn,
+  amount,
   spenderAddress,
 }: {
   address: `0x${string}`;
-  amountIn: BigNumber;
+  amount: BigNumber;
   spenderAddress: `0x${string}`;
 }) => {
-  const { chain } = useNetwork();
+  const { useForkEnv } = useFork();
+  const signer = useSigner();
+  const { address: account } = useAccount();
+
+  // Get allowance amount
+  const { data: allowanceAmount, refetch: refetchAllowance } = useContractRead({
+    address: address as `0x${string}`,
+    abi: erc20ABI,
+    functionName: 'allowance',
+    args: [account!, spenderAddress],
+  });
+
+  const contract = useContract({ address, abi: erc20ABI, signerOrProvider: signer });
   const { config: tokenConfig } = usePrepareContractWrite({
     address,
     abi: erc20ABI,
     functionName: 'approve',
-    args: [spenderAddress, amountIn],
+    args: [spenderAddress, amount],
   });
-  const { write: approvalWrite, data, isLoading, isSuccess } = useContractWrite(tokenConfig);
-  return { approvalWrite, data, isLoading, isSuccess };
+
+  const { writeAsync: approvalWriteAsync } = useContractWrite(tokenConfig);
+
+  const [hash, setHash] = useState<`0x${string}`>();
+
+  const approve = async () => {
+    if (useForkEnv) {
+      const tx = await contract?.connect(signer!).approve(spenderAddress, amount);
+      setHash(tx?.hash as `0x${string}`);
+    } else {
+      const tx = await approvalWriteAsync?.();
+      setHash(tx?.hash);
+    }
+  };
+
+  const { data, isError, isLoading, isSuccess } = useWaitForTransaction({
+    hash,
+    onSuccess: () => refetchAllowance(),
+  });
+
+  return {
+    approve,
+    data,
+    isLoading,
+    isError,
+    isSuccess,
+    hash,
+    allowanceAmount,
+    hasAllowance: allowanceAmount?.gte(amount),
+    refetchAllowance,
+  };
 };
 
 export default useTokenApproval;
