ac9ad51b597d456287f6e4a66c5c4751c9bd5592
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-30 08:10:25 -0700

    chore: condense

--- src/hooks/useParseMessage.tsx
+++ src/hooks/useParseMessage.tsx
@@ -1,10 +1,5 @@
 import { useMemo } from 'react';
 import { parseMessage } from '@/utils/parse-message';
 
-const useParseMessage = (message: string) => {
-  return useMemo(() => {
-    return parseMessage(message);
-  }, [message]);
-};
-
+const useParseMessage = (message: string) => useMemo(() => parseMessage(message), [message]);
 export default useParseMessage;
