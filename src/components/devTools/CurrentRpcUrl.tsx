16dfd1993202f8ee513db8fda1bde6b519004445
brucedonovan <bruce.donovan@gmail.com>
2023-03-30 11:30:35 +0200

    feat/currentRpc tool

---
+++ src/components/devTools/CurrentRpcUrl.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import {
+  ArrowTopRightOnSquareIcon,
+  CheckCircleIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+import copy from 'copy-to-clipboard';
+import { Button } from '@/components/Button';
+import useCachedState from '@/hooks/useCachedState';
+
+export const CurrentRpcUrl = () => {
+  const [url] = useCachedState('forkUrl');
+  const [tenderlyUrl, setTenderlyUrl] = useState<string>(url);
+  const [copied, setCopied] = useState<boolean>(false);
+
+  /* set a timer controlling it has been copied */
+  useEffect(() => {
+    copied && (async () => setTimeout(() => setCopied(false), 5000))();
+  }, [copied]);
+
+  /* Monitor rpc url and update link to tenderly TODO: increase robustness here */
+  useEffect(() => {
+    if (url) {
+      const forkId = url.split('/')[4];
+      setTenderlyUrl(`https://dashboard.tenderly.co/${process.env.TENDERLY_USER}/${process.env.TENDERLY_PROJECT}/fork/${forkId}`);
+    }
+  }, [url]);
+
+  const copyUrl = () => {
+    setCopied(true);
+    copy(url);
+  };
+
+  const goToTenderly = () => {
+    window.open(tenderlyUrl, '_blank', 'noopener,noreferrer');
+  };
+
+  return (
+    <div>
+      {/* <div className="text-left p-2 "> Current RPC URL </div> */}
+      <div className="border">
+        <div className="p-4 font-mono text-xs">{url}</div>
+        
+        <div className="flex gap-2 p-2 text-xs">
+
+          <Button onClick={copyUrl} disabled={copied}>
+            <div className="flex gap-2 text-xs">
+              <div className="w-6">
+                {copied ? <CheckCircleIcon /> : <DocumentDuplicateIcon />}{' '}
+              </div>
+              Copy RPC
+            </div>
+          </Button>
+
+          <Button onClick={goToTenderly} disabled={!url} >
+              <div className="flex gap-2 text-xs">
+                <div className="w-6">
+                  <ArrowTopRightOnSquareIcon />
+                </div>
+                Tenderly
+              </div>
+          </Button>
+        </div>
+
+      </div>
+    </div>
+  );
+};
