7e8b4a26f6d0db7dab7a5033b2e6f4534d74ebb5
brucedonovan <bruce.donovan@gmail.com>
2023-03-30 15:20:24 +0200

    validation of rpc

--- src/components/devTools/ChangeForkId.tsx
+++ src/components/devTools/ChangeForkId.tsx
@@ -1,47 +1,62 @@
 import { useCallback, useEffect, useState } from 'react';
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import { ArrowPathIcon, LockClosedIcon, XCircleIcon } from '@heroicons/react/24/outline';
 import axios from 'axios';
 import { Button } from '@/components/Button';
 import useCachedState from '@/hooks/useCachedState';
 
 export const ChangeForkId = () => {
   const [newUrl, setNewUrl] = useState<string>('');
-  const [validUrl, setValidUrl] = useState<boolean>(false);
+  const [validUrl, setValidUrl] = useState<boolean>();
   const [, setForkUrl] = useCachedState('forkUrl');
 
-  const rpcUrlFromProjectId = (id: string) => {
-    return `https://rpc.tenderly.co/fork/${id}`;
+  /* Get the project id from a valid RPC URL. TODO make this more robust*/
+  const parseIdfromUrl = (url: string): string => url.split('/')[4];
+
+  /* Parse the url from either a projectId or a url itself */
+  const parseRpcUrl = (urlOrId: string): string => {
+    if (urlOrId.includes('https://rpc.tenderly.co/')) return urlOrId;
+    return `https://rpc.tenderly.co/fork/${urlOrId}`;
   };
 
-  const checkForValidFork = async (forkId: string): Promise<boolean> => {
+  const isValidFork = async (forkId: string): Promise<boolean> => {
     const forkAPI = `https://api.tenderly.co/api/v1/account/${process.env.NEXT_PUBLIC_TENDERLY_USER}/project/${process.env.NEXT_PUBLIC_TENDERLY_PROJECT}/fork/${forkId}`;
-    const resp = await axios.get(forkAPI, {
-      headers: {
-        'X-Access-Key': process.env.NEXT_PUBLIC_TENDERLY_ACCESS_KEY as string,
-      },
-    }).catch((e) => { return e });
-    return resp.status === 201 ? true:false ;
+    const resp = await axios
+      .get(forkAPI, {
+        headers: {
+          'X-Access-Key': process.env.NEXT_PUBLIC_TENDERLY_ACCESS_KEY as string,
+        },
+      })
+      .catch((e) => {
+        return e;
+      });
+    // resp.status === 201 ? setValidUrl(true) : setValidUrl(false);
+    return resp.status === 201 ? true : false;
   };
 
   const apply = async () => {
-    /**
-     * If the input added is a valid url, use it as is. 
-     * Else, If the input appears to be a fork id, append it to the base tenderly url.
-    */
-    const parsedUrl = newUrl.includes('https://') ? newUrl : rpcUrlFromProjectId(newUrl);
-    const forkId = parsedUrl.split('/')[4];
+    /* get the input value as a url, if it isn't already */
+    const parsedUrl = parseRpcUrl(newUrl);
+    /* Check if we can get a valid response from fork at the parsed Url */
+    const isValid = await isValidFork(parseIdfromUrl(parsedUrl));
 
-    /* Check if we can get a valid response from the parsed Url */
-    const isValid = await checkForValidFork(forkId);
-
+    /* if valid, set the state to true and reload the page */
     if (isValid) {
       setValidUrl(true);
       setForkUrl(parsedUrl);
       window.location.reload();
-    } else {
-      setValidUrl(false);
     }
+    /* if not valid, set the validUrl state to false */
+    setValidUrl(false);
   };
+
+  /* When the newURL changes, check if the parse url is a valid fork */
+  useEffect(() => {
+    if (newUrl.length > 1) {
+      const _parsedUrl = parseRpcUrl(newUrl);
+      const _id = parseIdfromUrl(_parsedUrl);
+      isValidFork(_id).then((res: boolean) => setValidUrl(res));
+    }
+  }, [newUrl]);
 
   return (
     <div>
@@ -55,21 +70,29 @@ export const ChangeForkId = () => {
             type="text"
             placeholder="New fork ID or RPC URL"
           />
+          <div className="w-8 text-red-500">
+            {validUrl || newUrl === '' ? null : (
+              <div className="w-8">
+                <XCircleIcon />
+              </div>
+            )}
+          </div>
         </div>
 
         <div className="flex gap-2 p-2 text-xs">
           <Button
             onClick={apply}
-            className={`first-line:text-xs disabled:bg-gray-400 `}
-            disabled={newUrl === ''}
+            className={`first-line:text-xs disabled:bg-gray-400 ${validUrl && 'bg-green-500'} `}
+            disabled={!validUrl}
           >
             Apply New Fork URL
           </Button>
           <div className=" w-1/5">
             <Button
-              onClick={() =>
-                setNewUrl(rpcUrlFromProjectId(process.env.NEXT_PUBLIC_TENDERLY_FORK_ID))
-              }
+              onClick={() => {
+                const url = parseRpcUrl(process.env.NEXT_PUBLIC_TENDERLY_FORK_ID);
+                setNewUrl(url);
+              }}
               className="text-xs disabled:bg-gray-400"
             >
               <div className="w-4">
