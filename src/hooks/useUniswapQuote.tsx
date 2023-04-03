5a43f62e58a54b7dca8fca37dbc85cb8bee7a575
marcomariscal <marco.a.mariscal@gmail.com>
2023-04-03 12:42:05 -0700

    feat: use chain id

--- src/hooks/useUniswapQuote.tsx
+++ src/hooks/useUniswapQuote.tsx
@@ -2,36 +2,32 @@ import { CurrencyAmount, Token, TradeType } from '@uniswap/sdk-core';
 import { AlphaRouter } from '@uniswap/smart-order-router';
 import { ethers } from 'ethers';
 import useSWR from 'swr';
-import { useNetwork, useProvider } from 'wagmi';
+import { useProvider } from 'wagmi';
+import useChainId from '@/hooks/useChainId';
 import { cleanValue, findTokenBySymbol } from '@/utils';
-import { MAINNET_CHAIN_ID } from '@/utils/constants';
-import useChainId from './useChainId';
-
-const QUOTER_CONTRACT_ADDRESS = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6';
 
 const useUniswapQuote = (props: {
   baseTokenSymbol: string;
   quoteTokenSymbol: string;
   amount?: string;
 }) => {
-  const { chain } = useNetwork();
+  const chainId = useChainId();
   const provider = useProvider();
-  const CHAIN_ID_FORK_FALLBACK = chain?.id || 36963;
 
   const { isLoading, error, data } = useSWR(
-    `useUniswapQuote-${props.baseTokenSymbol}-${props.quoteTokenSymbol}-${chain?.id || '1'}`,
+    `useUniswapQuote-${props.baseTokenSymbol}-${props.quoteTokenSymbol}-${chainId}`,
     async () => {
       const isQueryTokenEth = props.quoteTokenSymbol === 'ETH';
       const isBaseTokenEth = props.baseTokenSymbol === 'ETH';
       const tokenIn = isBaseTokenEth
-        ? findTokenBySymbol('WETH', CHAIN_ID_FORK_FALLBACK)
-        : findTokenBySymbol(props.baseTokenSymbol, CHAIN_ID_FORK_FALLBACK);
+        ? findTokenBySymbol('WETH', chainId)
+        : findTokenBySymbol(props.baseTokenSymbol, chainId);
       const tokenOut = isQueryTokenEth
-        ? findTokenBySymbol('WETH', CHAIN_ID_FORK_FALLBACK)
-        : findTokenBySymbol(props.quoteTokenSymbol, CHAIN_ID_FORK_FALLBACK);
+        ? findTokenBySymbol('WETH', chainId)
+        : findTokenBySymbol(props.quoteTokenSymbol, chainId);
       const router = new AlphaRouter({
-        chainId: MAINNET_CHAIN_ID,
-        provider: provider,
+        chainId,
+        provider,
       });
 
       if (!tokenIn) throw new Error(`Token ${props.baseTokenSymbol} not found`);
@@ -41,23 +37,11 @@ const useUniswapQuote = (props: {
 
       const route = await router.route(
         CurrencyAmount.fromRawAmount(
-          new Token(
-            MAINNET_CHAIN_ID,
-            tokenIn.address,
-            tokenIn.decimals,
-            tokenIn.symbol,
-            tokenIn.name
-          ),
+          new Token(chainId, tokenIn.address, tokenIn.decimals, tokenIn.symbol, tokenIn.name),
 
           ethers.utils.parseUnits(amountToUse || '1', tokenIn.decimals).toString()
         ),
-        new Token(
-          MAINNET_CHAIN_ID,
-          tokenOut.address,
-          tokenOut.decimals,
-          tokenOut.symbol,
-          tokenOut.name
-        ),
+        new Token(chainId, tokenOut.address, tokenOut.decimals, tokenOut.symbol, tokenOut.name),
         TradeType.EXACT_INPUT
       );
 
