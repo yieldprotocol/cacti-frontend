81f734e44caf20b41022efc0086ad3a52512cf14
Sagar Shah <shah.sagar.nilesh@gmail.com>
2023-04-05 16:01:44 -0500

    wip

--- src/components/MessageTranslator.tsx
+++ src/components/MessageTranslator.tsx
@@ -27,6 +27,7 @@ import {
   NftsWithAttributes,
 } from './widgets/NftAttributes';
 import { NftSearch } from './widgets/NftSearch';
+import { SendTransaction } from './widgets/SendTransaction';
 import { YieldFarm } from './widgets/YieldFarm';
 import { YieldRowContainer } from './widgets/YieldRowContainer';
 import { ActionPanel } from './widgets/helpers/ActionPanel';
@@ -320,7 +321,7 @@ const Widgetize = (widget: Widget, chain: Chain) => {
             <thead className="bg-gray-800 text-left">
               <tr className="border-b border-gray-400">
                 {headers.map(({ displayName }: { displayName: string }, i: number) => (
-                  <th className="py-1 px-2" key={`i${i}`}>
+                  <th className="px-2 py-1" key={`i${i}`}>
                     {displayName}
                   </th>
                 ))}
@@ -340,6 +341,24 @@ const Widgetize = (widget: Widget, chain: Chain) => {
               })}
             </tbody>
           </table>
+        );
+      }
+      case 'transaction-for-signing-container': {
+        const params = JSON.parse(args);
+        console.log('params', params);
+        return (
+          <ActionPanel
+            header={params.description}
+            msg={inputString}
+            key={inputString}
+            centerTitle={true}
+          >
+            <div className="flex w-[100%] justify-end">
+              <ConnectFirst>
+                <SendTransaction {...params} />
+              </ConnectFirst>
+            </div>
+          </ActionPanel>
         );
       }
       default:
