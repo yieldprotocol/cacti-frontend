394c735c512e47ffc30c8c6b2928e997d09ec7d7
brucedonovan <bruce.donovan@gmail.com>
2023-04-21 10:15:01 +0100

    include ss boiler

--- src/components/devTools/SessionInfo.tsx
+++ src/components/devTools/SessionInfo.tsx
@@ -1,10 +1,12 @@
 import { shortenAddress } from '@/utils';
 import { useSession } from 'next-auth/react';
+import { getToken } from 'next-auth/jwt';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { getSession } from 'next-auth/react';
 
 export const SessionInfo = () => {
 
   const { data, status } = useSession();
-
   return (
     <div>
       { data?.user && <div  className='text-sm text-left'> user: {shortenAddress(data?.user?.name || '')}</div> }
@@ -12,3 +14,29 @@ export const SessionInfo = () => {
     </div>
   );
 };
+
+// export const getServerSideProps: GetServerSideProps = async context => {
+//   const session = await getSession(context);
+//   const token = await getToken({ req: context.req });
+//   const address = token?.sub ?? null;
+//   // If you have a value for "address" here, your
+//   // server knows the user is authenticated.
+//   // You can then pass any data you want
+//   // to the page component here.
+//   return {
+//     props: {
+//       address,
+//       session,
+//     },
+//   };
+// };
+
+// type AuthenticatedPageProps = InferGetServerSidePropsType<
+//   typeof getServerSideProps
+// >;
+
+// const AuthenticatedAddress = ({
+//   address,
+// }: AuthenticatedPageProps) => {
+//   return address
+// }
