73101b086eada348824f9f023ba7b4b8edb74062
marcomariscal <marco.a.mariscal@gmail.com>
2023-04-24 12:51:43 -0700

    feat: use message input component

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,19 +1,18 @@
 import { ReactNode } from 'react';
-import { TrashIcon } from '@heroicons/react/24/outline';
 import Avatar from '@/components/Avatar';
 import { FeedbackButton } from '@/components/FeedbackButton';
 import { MessageTranslator } from '@/components/MessageTranslator';
 import { SystemMessage } from '@/components/SystemMessage';
-import { UserMessage } from '@/components/UserMessage';
-import { Message, useChatContext } from '@/contexts/ChatContext';
-import { ActionType, Actor } from '@/types/chat';
+import { Message } from '@/contexts/ChatContext';
+import { Actor } from '@/types/chat';
+import MessageInput from './MessageInput';
 
 export const MessageItemWrap = ({ actor, children }: { actor: string; children: ReactNode }) => {
   return (
     <div
       className={`
       m-auto flex items-start gap-4 p-4 px-4 py-4 text-base md:gap-6 md:px-40 lg:px-64
-       ${actor != Actor.USER ? 'bg-gray-600 text-white' : 'bg-gray-700 text-white'}`}
+       ${actor !== Actor.USER ? 'bg-gray-600 text-white' : 'bg-gray-700 text-white'}`}
     >
       {children}
     </div>
@@ -32,11 +31,7 @@ export const MessageItem = ({ message }: { message: Message }) => {
         ) : actor === Actor.SYSTEM ? (
           <SystemMessage message={payload} />
         ) : (
-          <UserMessage
-            {...{
-              message,
-            }}
-          />
+          <MessageInput message={message} />
         )}
       </div>
       {actor === Actor.BOT && <FeedbackButton message={message} />}
