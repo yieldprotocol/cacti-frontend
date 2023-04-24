7dfdfc951d2c9766ce8ffaa1ce9f3be3a624ff20
marcomariscal <marco.a.mariscal@gmail.com>
2023-04-24 09:27:55 -0700

    feat: type and style

--- src/components/FeedbackButton.tsx
+++ src/components/FeedbackButton.tsx
@@ -1,18 +1,11 @@
 import { useState } from 'react';
 import { HandThumbDownIcon, HandThumbUpIcon } from '@heroicons/react/24/outline';
 import { Message, useChatContext } from '@/contexts/ChatContext';
+import { ActionType } from '@/types/chat';
 
-const Button = ({
-  label,
-  feedback,
-  onClick,
-}: {
-  label: string;
-  feedback: string;
-  onClick: () => void;
-}) => {
+const Button = ({ label, onClick }: { label: string; feedback: string; onClick: () => void }) => {
   return (
-    <div className="mt-2 flex justify-center gap-3 self-end text-gray-400 md:gap-4 lg:mt-0 lg:translate-x-full lg:gap-1 lg:self-center lg:pl-2">
+    <div className="flex justify-center text-gray-400">
       <button
         onClick={onClick}
         className="rounded-md p-1 text-gray-100 hover:bg-gray-700 hover:text-gray-200 enabled:hover:cursor-pointer disabled:bg-gray-600 disabled:hover:text-gray-400"
@@ -35,13 +28,13 @@ export const FeedbackButton = ({ message }: { message: Message }) => {
   const sendFeedback = (label: string) => {
     const choice = feedback === 'none' ? label : 'none';
     setFeedback(choice);
-    sendAction({ actionType: 'feedback', messageId, choice });
+    sendAction({ actionType: ActionType.FEEDBACK, messageId, choice });
   };
 
   return feedback === 'n/a' ? (
     <></>
   ) : (
-    <div className="flex justify-between">
+    <div className="flex justify-between gap-1">
       {(feedback === 'none' || feedback === 'good') && (
         <Button onClick={() => sendFeedback('good')} label="good" feedback={feedback} />
       )}
