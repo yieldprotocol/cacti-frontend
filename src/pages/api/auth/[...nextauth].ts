8faf20dfe0022e9d45b57076b16d80699578f429
brucedonovan <bruce.donovan@gmail.com>
2023-04-24 12:28:24 +0100

    working with JWTs only

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,12 +10,9 @@ import PostgresAdapter from "@/utils/postgres_authAdapter";
 // https://next-auth.js.org/configuration/options
 export default async function auth(req: any, res: any) {
 
+  const connectionString = "postgresql://localhost/chatdb"
   const client = new Pool({
-    user: 'chatdb',
-    host: 'localhost',
-    database: 'chatdb',
-    password: 'secretpassword',
-    port: 5432,
+    connectionString,
   });
 
   const providers = [
@@ -37,13 +34,11 @@ export default async function auth(req: any, res: any) {
         try {
           const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"))
           const nextAuthUrl = new URL(process.env.NEXTAUTH_URL!)
-
           const result = await siwe.verify({
             signature: credentials?.signature || "",
             domain: nextAuthUrl.host,
             nonce: await getCsrfToken({ req }),
           })
-
           if (result.success) {
             return {
               id: siwe.address,
@@ -66,14 +61,10 @@ export default async function auth(req: any, res: any) {
   }
 
   return await NextAuth(req, res, {
-    // https://next-auth.js.org/configuration/providers/oauth
-    // adapters:{
-    //   MongoDb: MongoDbAdapter
-    // },
     adapter: PostgresAdapter(client),
     providers,
     session: {
-      strategy: "database",
+      strategy: "jwt",
     },
     secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
