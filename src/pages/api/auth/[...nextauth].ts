ce011e218b7ab3e115f7cc41d1b59fa475a23086
brucedonovan <bruce.donovan@gmail.com>
2023-04-25 12:54:24 +0100

    remove postgres

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,22 +3,13 @@ import CredentialsProvider from "next-auth/providers/credentials"
 import { getCsrfToken } from "next-auth/react"
 import { SiweMessage } from "siwe"
 
-import { Pool } from "pg";
-import PostgresAdapter from "@/utils/postgres_authAdapter";
-
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default async function auth(req: any, res: any) {
 
-  const connectionString = "postgresql://localhost/chatdb"
-  const client = new Pool({
-    connectionString,
-  });
-
   const providers = [
-
+    
     // OAuth authentication providers...
-
     CredentialsProvider({
       name: "Ethereum",
       credentials: {
@@ -35,13 +26,37 @@ export default async function auth(req: any, res: any) {
       },
       async authorize(credentials) {
         try {
+
           const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"))
           const nextAuthUrl = new URL(process.env.NEXTAUTH_URL!)
+          
           const result = await siwe.verify({
             signature: credentials?.signature || "",
             domain: nextAuthUrl.host,
             nonce: await getCsrfToken({ req }),
           })
+
+          
+          
+          // const res = await fetch(`${BACKEND_ADDR!}/nonce`|| '', {
+          //   credentials: 'include',
+          // });
+
+            const message = new SiweMessage({
+                domain: nextAuthUrl.host,
+                address: siwe.address,
+                statement: credentials?.message,
+                uri: origin,
+                version: '1',
+                chainId: 1,
+                nonce: await res.text()
+            });
+
+            console.log( message.prepareMessage())
+
+            // console.log(message.prepareMessage());
+            // return message.prepareMessage();
+
           if (result.success) {
             return {
               id: siwe.address,
@@ -54,7 +69,6 @@ export default async function auth(req: any, res: any) {
       },
     }),
 
-
   ]
 
   const isDefaultSigninPage =
@@ -66,24 +80,17 @@ export default async function auth(req: any, res: any) {
   }
 
   return await NextAuth(req, res, {
-    // adapter: PostgresAdapter(client),
     providers,
     session: {
       strategy: "jwt",
     },
     secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
-      // async jwt({token, profile} : any) {
-      //   profile && console.log('profile', profile )
-      //   console.log('token', token )
-      //   return token;
-      // },
-
-      async session({ session, token }: { session: any; token: any }) { 
+      async session({ session, token, user }: { session: any; token: any, user:any }) { 
         session.address = token.sub
         session.user.name = token.sub
         session.jti = token.jti
-        console.log('session', session)
+        console.log('Session:', session)
         return session 
       },
 
