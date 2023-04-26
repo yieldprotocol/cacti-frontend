c51db81d34b68c94ec86f9ef93acfe7e3408e57c
brucedonovan <bruce.donovan@gmail.com>
2023-04-26 11:17:49 +0100

    feat/fork button activate

--- src/components/devTools/ForkButton.tsx
+++ src/components/devTools/ForkButton.tsx
@@ -1,15 +1,21 @@
 import { Switch } from '@headlessui/react';
 import { SignalIcon } from '@heroicons/react/20/solid';
-import useForkTools from '@/hooks/useForkTools';
-import useShortcutKey from '@/hooks/useShortcutKey';
+import SettingsContext, { Setting } from '@/contexts/SettingsContext';
+import { useContext } from 'react';
 
 const ForkButton = () => {
-  const { isFork, setIsFork } = useForkTools();
+
+  const {settings , changeSetting} = useContext(SettingsContext);
+  const { isForkedEnv } = settings;
+
+  const setIsFork = (val: boolean) => {
+    changeSetting( Setting.FORKED_ENV , val);
+  }
 
   return (
     <button
       className="text-md flex w-full items-center gap-2 rounded-xl p-2 hover:bg-slate-600"
-      onClick={() => setIsFork(!isFork)}
+      onClick={() => setIsFork(!isForkedEnv)}
     >
       <SignalIcon className="ml-1 h-5 w-5" />
       <div className="flex w-full items-center justify-between hover:text-white">
@@ -17,15 +23,15 @@ const ForkButton = () => {
         <div className="align-middle font-mono text-xs text-slate-500"> ( alt-f )</div>
 
         <Switch
-          checked={isFork}
+          checked={isForkedEnv}
           onChange={setIsFork}
           className={`${
-            isFork ? 'bg-green-600' : 'bg-gray-600'
+            isForkedEnv ? 'bg-green-600' : 'bg-gray-600'
           } relative inline-flex h-6 w-11 items-center rounded-full`}
         >
           <span
             className={`${
-              isFork ? 'translate-x-6' : 'translate-x-1'
+              isForkedEnv ? 'translate-x-6' : 'translate-x-1'
             } inline-block h-4 w-4 transform rounded-full bg-white transition`}
           />
         </Switch>
