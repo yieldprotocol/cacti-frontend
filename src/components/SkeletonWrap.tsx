ddde1994f6e255ca4e658b9c131296cfa7b83b1c
brucedonovan <bruce.donovan@gmail.com>
2023-05-15 11:33:20 +0100

    skeleton handle experimental ui

--- src/components/SkeletonWrap.tsx
+++ src/components/SkeletonWrap.tsx
@@ -1,7 +1,14 @@
+import SettingsContext, { Setting } from '@/contexts/SettingsContext';
+import { useContext } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
-const SkeletonWrap = (props: any) => (
-  <Skeleton {...props} baseColor="#4b5563" highlightColor="#6b7280" />
-);
+const SkeletonWrap = (props: any) => {
+  const {settings:{experimentalUi}} = useContext(SettingsContext);
+  return (
+    experimentalUi 
+    ? <Skeleton {...props} baseColor="#ffffff01" highlightColor="#00808050" /> //rgba(139, 0, 0, 0.5)
+    : <Skeleton {...props} baseColor="#4b5563" highlightColor="#6b7280" />
+  );
+} 
 
 export default SkeletonWrap;
