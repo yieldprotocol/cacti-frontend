7cd5667682aba7f55da789112525f7149648c855
brucedonovan <bruce.donovan@gmail.com>
2023-05-16 08:14:47 +0100

    attemp to get font decoratioing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: [
+        "Inter var, sans-serif",
+        {
+          fontFeatureSettings: '"zero", "lnum" on, "pnum" on',
+        },
+      ],
+    },
     extend: {
       animation: {
         ellipse: 'dotty steps(1,end) 1s infinite',
