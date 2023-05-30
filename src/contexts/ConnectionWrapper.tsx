bf7e3bfde842ff0c06a52fbf22aaa6418e940c82
Sagar Shah <shah.sagar.nilesh@gmail.com>
2023-05-30 14:54:15 -0500

    wip

--- src/contexts/ConnectionWrapper.tsx
+++ src/contexts/ConnectionWrapper.tsx
@@ -84,24 +84,6 @@ const ConnectionWrapper = ({ children, pageProps }: any) => {
     await axios.post(`${backendUrl}/logout`, {}, { withCredentials: true });
   };
 
-  const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
-    const color = '#1f2937';
-    return ensImage ? (
-      <img src={ensImage} width={size} height={size} style={{ borderRadius: 999 }} />
-    ) : (
-      <div
-        style={{
-          backgroundColor: color,
-          borderRadius: 999,
-          height: size,
-          width: size,
-        }}
-      >
-        :^
-      </div>
-    );
-  };
-
   return (
     <WagmiConfig client={wagmiClient}>
       <SessionProvider refetchInterval={0} session={pageProps?.session}>
@@ -115,7 +97,6 @@ const ConnectionWrapper = ({ children, pageProps }: any) => {
             chains={chains}
             theme={lightTheme({ accentColor: '#1f2937' })}
             showRecentTransactions={true}
-            avatar={CustomAvatar}
           >
             {children}
           </RainbowKitProvider>
