73f45f86c1a9feed28bbca4578845875842d97d8
marcomariscal <marco.a.mariscal@gmail.com>
2023-05-31 11:58:24 -0700

    feat: use ens avatar as user avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,32 +1,44 @@
-import Image from 'next/image';
-import { UserIcon } from '@heroicons/react/20/solid';
+import { jsNumberForAddress } from 'react-jazzicon';
+import Jazzicon from 'react-jazzicon/dist/Jazzicon';
 import { ClipboardDocumentListIcon, CommandLineIcon } from '@heroicons/react/24/outline';
-import profilePic from '@/public/punk2042.png';
+import { useAccount, useEnsAvatar } from 'wagmi';
 
 interface Props {
   actor: string;
 }
 
+export const UserAvatar = ({ address, size }: { address: `0x${string}`; size: number }) => {
+  const { data: ensImage } = useEnsAvatar({ address: address as `0x${string}` });
+  return ensImage ? (
+    <img
+      alt="avatar"
+      src={ensImage}
+      className={`h-[${size}px] w-[${size}px] max-w-none rounded-full`}
+    />
+  ) : (
+    <Jazzicon diameter={size} seed={jsNumberForAddress(address)} />
+  );
+};
+
 const Avatar = ({ actor }: Props) => {
+  const { address } = useAccount();
+  const avatarSize = 40;
+  const avatarSizeStyle = `h-[${avatarSize}px] w-[${avatarSize}px] max-w-none`;
   const botAvatar =
     'https://user-images.githubusercontent.com/1568680/221064265-c6d3b2be-148b-4bec-b955-e6f59be9e0ef.png';
 
   return (
-    <div className="rounded-md bg-gray-300 shadow-md">
+    <div>
       {actor === 'user' ? (
-        <Image
-          width={32}
-          height={32}
-          className="h-full w-8 max-w-none rounded-md"
-          src={profilePic}
-          alt="bot avatar"
-        />
+        <UserAvatar address={address!} size={avatarSize} />
       ) : actor === 'system' ? (
-        <CommandLineIcon className="h-8 w-8 text-black" />
+        <CommandLineIcon className={`${avatarSizeStyle} rounded-md bg-gray-300 text-black`} />
       ) : actor === 'commenter' ? (
-        <ClipboardDocumentListIcon className="h-8 w-8 text-black" />
+        <ClipboardDocumentListIcon
+          className={`${avatarSizeStyle} rounded-md bg-gray-300 text-black`}
+        />
       ) : (
-        <img className="h-full w-8 max-w-none rounded-md" src={botAvatar} alt="bot avatar" />
+        <img className={`${avatarSizeStyle} rounded-full`} src={botAvatar} alt="bot avatar" />
       )}
     </div>
   );
