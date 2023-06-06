42f96fd3112538aeb7fa9a73cb4cc5cd5611a302
marcomariscal <marco.a.mariscal@gmail.com>
2023-06-06 14:23:30 -0700

    fix: render order

--- src/components/experimental_/widgets/yield-protocol/actions/lend/YieldProtocolLend.tsx
+++ src/components/experimental_/widgets/yield-protocol/actions/lend/YieldProtocolLend.tsx
@@ -20,11 +20,10 @@ import { getTxParams } from '../../helpers';
 import { nameFromMaturity } from '../../utils';
 import lend from './helpers';
 
-// Yield Protocol specific ladle addresses based on chain id
-const YIELD_PROTOCOL_LADLES: { [chainId: number]: Address } = {
-  1: '0x6cb18ff2a33e981d1e38a663ca056c0a5265066a',
-  42161: '0x16e25cf364cecc305590128335b8f327975d0560',
-};
+// arbitrary protocol operation custom data
+interface YieldLendRes {
+  maturity_: string; // formatted maturity date
+}
 
 // should be generalized and only needed as reference once for all components
 interface InputProps {
@@ -42,17 +41,6 @@ const YieldProtocolLend = ({
   action,
   projectName,
 }: InputProps) => {
-  ////////////////DON'T TOUCH//////////////////////////////////////
-  // generic weird input handling; can be abstracted out
-  if (inputAmount === '*' || inputAmount === '{amount}')
-    return (
-      <TextResponse
-        text={`Please edit your query with an amount you wish to trade on ${toTitleCase(
-          projectName
-        )}.`}
-      />
-    );
-
   /* generic hook usage (not to be touched when creating from example) */
   const chainId = useChainId();
   const { address } = useAccount();
@@ -61,32 +49,26 @@ const YieldProtocolLend = ({
   const label = `
         ${toTitleCase(action)} ${inputAmount} ${tokenInSymbol} on ${toTitleCase(projectName)}`;
   const amount = useInput(inputAmount, tokenInSymbol);
-  if (!amount) {
-    console.error('amount is undefined');
-    return null;
-  }
-  ////////////////DON'T TOUCH//////////////////////////////////////
 
   /***************INPUTS******************************************/
+
+  const [approvalParams, setApprovalParams] = useState<ApprovalBasicParams>();
+  const [txParams, setTxParams] = useState<TxBasicParams>();
+  const [data, setData] = useState<YieldLendRes>();
+
   // Yield Protocol specific spender handling
   const ladle = contractAddresses.addresses.get(chainId)?.get(ContractNames.LADLE);
-  const approvalParams: ApprovalBasicParams = {
-    address: tokenIn?.address!,
-    spender: ladle!,
-    amount,
-  }; // TODO handle no token or ladle addresses more gracefully
-
-  const [txParams, setTxParams] = useState<TxBasicParams>();
-
-  // arbitrary protocol operation data
-  interface YieldLendRes {
-    maturity_: string; // formatted maturity date
-  }
-  const [data, setData] = useState<YieldLendRes>();
 
   // set tx params (specific to yield protocol)
   useEffect(() => {
     (async () => {
+      const approvalParams: ApprovalBasicParams = {
+        address: tokenIn?.address!,
+        spender: ladle!,
+        amount: amount!,
+      }; // TODO handle no token or ladle addresses more gracefully
+      setApprovalParams(approvalParams);
+
       const baseAddress = tokenIn?.address as Address | undefined;
       if (!baseAddress) return;
       const seriesEntities = SERIES_ENTITIES.get(chainId);
@@ -101,13 +83,23 @@ const YieldProtocolLend = ({
       const poolAddress = seriesEntity?.poolAddress;
       if (!poolAddress) return;
 
-      const lendCallData = lend(address, amount, baseAddress, poolAddress, tokenInIsETH);
+      const lendCallData = lend(address, amount!, baseAddress, poolAddress, tokenInIsETH);
 
-      setTxParams(lendCallData ? await getTxParams(lendCallData, ladle) : undefined);
+      setTxParams(lendCallData ? await getTxParams(lendCallData, ladle!) : undefined);
       setData({ maturity_: `${nameFromMaturity(seriesEntity.maturity, 'MMM yyyy')}` });
     })();
-  }, []);
+  }, [address, amount, chainId, ladle, tokenIn?.address, tokenInIsETH]);
   /***************INPUTS******************************************/
+
+  // generic weird input handling; can be abstracted out
+  if (inputAmount === '*' || inputAmount === '{amount}')
+    return (
+      <TextResponse
+        text={`Please edit your query with an amount you wish to trade on ${toTitleCase(
+          projectName
+        )}.`}
+      />
+    );
 
   return (
     <>
