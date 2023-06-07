31c2eb6e5281f6a7c86447a28195a1775bcfd494
Ali <ali.hussein.pre@gmail.com>
2023-06-07 11:50:52 +0200

    style: make icon-btn style consistent

--- src/components/FeedbackButton.tsx
+++ src/components/FeedbackButton.tsx
@@ -1,34 +1,10 @@
 import { useState } from 'react';
 import { HandThumbDownIcon, HandThumbUpIcon } from '@heroicons/react/24/outline';
 import { Message, useChatContext } from '@/contexts/ChatContext';
-
-const Button = ({
-  label,
-  feedback,
-  onClick,
-}: {
-  label: string;
-  feedback: string;
-  onClick: () => void;
-}) => {
-  return (
-    <div className="mt-2 flex justify-center gap-3 self-end text-gray-400 md:gap-4 lg:mt-0 lg:translate-x-full lg:gap-1 lg:self-center lg:pl-2">
-      <button
-        onClick={onClick}
-        className="rounded-md p-1 text-gray-100 hover:bg-gray-700 hover:text-gray-200 enabled:hover:cursor-pointer disabled:bg-gray-600 disabled:hover:text-gray-400"
-      >
-        {label === 'good' ? (
-          <HandThumbUpIcon className="h-4 w-4" />
-        ) : (
-          <HandThumbDownIcon className="h-4 w-4" />
-        )}
-      </button>
-    </div>
-  );
-};
+import { IconBtn } from './shared/IconBtn';
 
 export const FeedbackButton = ({ message }: { message: Message }) => {
-  const { messageId, actor, feedback: initialFeedback } = message;
+  const { messageId, feedback: initialFeedback } = message;
   const [feedback, setFeedback] = useState(initialFeedback);
   const { sendAction } = useChatContext();
 
@@ -41,12 +17,16 @@ export const FeedbackButton = ({ message }: { message: Message }) => {
   return feedback === 'n/a' ? (
     <></>
   ) : (
-    <div className="flex justify-between">
+    <div className="flex flex-wrap gap-2 opacity-0 duration-200 group-hover:opacity-100 sm:flex-nowrap">
       {(feedback === 'none' || feedback === 'good') && (
-        <Button onClick={() => sendFeedback('good')} label="good" feedback={feedback} />
+        <IconBtn onClick={() => sendFeedback('good')}>
+          {<HandThumbUpIcon className="h-4 w-4" />}
+        </IconBtn>
       )}
       {(feedback === 'none' || feedback === 'bad') && (
-        <Button onClick={() => sendFeedback('bad')} label="bad" feedback={feedback} />
+        <IconBtn onClick={() => sendFeedback('bad')}>
+          <HandThumbDownIcon className="h-4 w-4" />
+        </IconBtn>
       )}
     </div>
   );
--- src/components/UserMessage.tsx
+++ src/components/UserMessage.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { ArrowPathIcon, PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import { IconBtn } from './shared/IconBtn';
 
 export const UserMessage = ({
   actor,
@@ -15,7 +17,6 @@ export const UserMessage = ({
   submitDelete: () => void;
 }) => {
   const [input, setInput] = useState(initialText);
-  const [hovered, setHovered] = useState(true);
   const [isEditing, setIsEditing] = useState(false);
 
   const inputRef = useRef<HTMLInputElement>(null);
@@ -46,14 +47,12 @@ export const UserMessage = ({
 
   return (
     <div
-      className={`flex justify-between rounded-md bg-gray-700
-    hover:bg-gray-700/20 hover:ring-1
-      hover:ring-gray-500/80 focus:text-gray-50
-      focus:ring-gray-500/80
+      className={`flex items-center justify-between rounded-md bg-gray-700
+    pr-1 hover:bg-gray-700/20
+      hover:ring-1 hover:ring-gray-500/80
+      focus:text-gray-50 focus:ring-gray-500/80
    ${isEditing ? 'ring-1 ring-gray-500/80' : ''}
    `}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
     >
       <input
         ref={inputRef}
@@ -79,21 +78,24 @@ export const UserMessage = ({
           </span>
         </div>
       )}
-      {hovered && !isEditing && (
-        <>
-          <button className="flex p-2" onClick={() => setIsEditing(true)}>
-            <PencilSquareIcon className="h-4 w-4" />
-          </button>
-          {actor === 'user' && (
-            <button className="flex p-2" onClick={submitRegenerate}>
-              <ArrowPathIcon className="h-4 w-4" />
-            </button>
-          )}
-          <button className="flex p-2" onClick={submitDelete}>
-            <TrashIcon className="h-4 w-4" />
-          </button>
-        </>
-      )}
+      <div
+        className={clsx(
+          'flex gap-1 opacity-0 duration-200 group-hover:opacity-100',
+          isEditing && 'hidden'
+        )}
+      >
+        <IconBtn onClick={() => setIsEditing(true)}>
+          <PencilSquareIcon className="h-4 w-4" />
+        </IconBtn>
+        {actor === 'user' && (
+          <IconBtn onClick={submitRegenerate}>
+            <ArrowPathIcon className="h-4 w-4" />
+          </IconBtn>
+        )}
+        <IconBtn onClick={submitDelete}>
+          <TrashIcon className="h-4 w-4" />
+        </IconBtn>
+      </div>
     </div>
   );
 };
