61c25f0651c54d7e466481313e294c4e8205822c
Ali <ali.hussein.pre@gmail.com>
2023-06-07 12:10:15 +0200

    style: handle content width

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import clsx from 'clsx';
 import Avatar from '@/components/Avatar';
 import { FeedbackButton } from '@/components/FeedbackButton';
 import { MessageTranslator } from '@/components/MessageTranslator';
@@ -9,9 +10,10 @@ import { Message, useChatContext } from '@/contexts/ChatContext';
 export const MessageItemWrap = ({ actor, children }: { actor: string; children: ReactNode }) => {
   return (
     <div
-      className={`
-      m-auto flex items-start gap-4 p-4 px-4 py-4 text-base md:gap-6 md:px-40 lg:px-64
-       ${actor != 'user' ? 'bg-gray-600 text-white' : 'bg-gray-700 text-white'}`}
+      className={clsx(
+        'group m-auto flex max-w-5xl items-start gap-2 p-4 text-base md:gap-6',
+        actor != 'user' ? 'bg-gray-600 text-white' : 'bg-gray-700 text-white'
+      )}
     >
       {children}
     </div>
@@ -46,7 +48,7 @@ export const MessageItem = ({ message }: { message: Message }) => {
   return (
     <MessageItemWrap actor={actor}>
       <Avatar actor={actor} />
-      <div className={`relative flex w-[100%] flex-col gap-1 md:gap-3 lg:w-[100%]`}>
+      <div className="relative flex grow flex-col gap-1 md:gap-3">
         {actor === 'bot' ? (
           <MessageTranslator message={payload} />
         ) : actor === 'system' ? (
