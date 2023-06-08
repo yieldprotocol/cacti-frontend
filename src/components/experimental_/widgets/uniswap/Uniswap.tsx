ed243b4e097b605863a1df10efef94c97db33db6
brucedonovan <bruce.donovan@gmail.com>
2023-06-08 17:58:49 +0100

    bugfix/uniswap short circuit

--- src/components/experimental_/widgets/uniswap/Uniswap.tsx
+++ src/components/experimental_/widgets/uniswap/Uniswap.tsx
@@ -38,13 +38,7 @@ interface ExactInputSingleParams {
 }
 
 const Uniswap = ({ tokenInSymbol, tokenOutSymbol, inputAmount }: UniswapProps) => {
-  if (inputAmount === '*' || inputAmount === '{amount}')
-    return (
-      <TextResponse text="Please edit your query with an amount you wish to trade on Uniswap." />
-    );
-  if (!tokenOutSymbol)
-    return <TextResponse text={`Please enter a valid token to trade on Uniswap`} />;
-
+  
   const chainId = useChainId();
 
   const { address: receiver } = useAccount();
@@ -80,6 +74,15 @@ const Uniswap = ({ tokenInSymbol, tokenOutSymbol, inputAmount }: UniswapProps) =
     amount: undefined,
   });
 
+  // shortcircuit if no input amount
+  if (inputAmount === '*' || inputAmount === '{amount}')
+  return (
+    <TextResponse text="Please edit your query with an amount you wish to trade on Uniswap." />
+  );
+  if (!tokenOutSymbol)
+  return <TextResponse text={`Please enter a valid token to trade on Uniswap`} />;
+
+
   const calcPrice = (quote: string | undefined, amount: string | undefined) =>
     !quote || !amount ? undefined : cleanValue((+quote / +amount).toString(), 2);
 
@@ -109,7 +112,7 @@ const Uniswap = ({ tokenInSymbol, tokenOutSymbol, inputAmount }: UniswapProps) =
 
   const approval = {
     address: tokenIn?.address as `0x${string}`,
-    amount: amountIn,
+    approvalAmount: amountIn,
     spender: SWAP_ROUTER_02_ADDRESSES(chainId),
   };
 
