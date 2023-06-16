1b6c41386b7255ff73848b394da0e2fec121fe51
brucedonovan <bruce.donovan@gmail.com>
2023-06-16 11:56:30 +0100

    bothinking style

--- src/components/experimental_/BotThinking.tsx
+++ src/components/experimental_/BotThinking.tsx
@@ -1,11 +1,27 @@
+import Avatar from '../Avatar';
 import { ResponseWrap } from '../cactiComponents/helpers/layout';
 
 export const BotThinking = () => {
   return (
-    <ResponseWrap>
-      <div className="relative h-[24px] overflow-hidden rounded-[6px] ">
-        <div className="botLight" />
+    <div className="grid-gap-2 mb-8 grid grid-cols-12 py-3">
+      <div className="col-span-2 py-4">
+        <div className="float-right">
+          <Avatar actor="bot" />
+        </div>
       </div>
-    </ResponseWrap>
+
+      <div className="col-span-8 flex 
+          h-full w-full flex-col 
+          gap-2 
+          px-4 
+          text-white/70
+          ">
+        <ResponseWrap>
+          <div className="relative h-[16px] overflow-hidden rounded-[6px] ">
+            <div className="botLight" />
+          </div>
+        </ResponseWrap>
+      </div>
+    </div>
   );
 };
