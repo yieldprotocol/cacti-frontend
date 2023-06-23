b734de572cb20a8b1b3aee9311e111c76e037226
Ali <ali.hussein.pre@gmail.com>
2023-06-23 22:31:51 +0200

    feat: show comments in markdown

--- src/components/experimental_/UserMessage_.tsx
+++ src/components/experimental_/UserMessage_.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import * as React from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import {
   PaperAirplaneIcon,
   PaperClipIcon,
@@ -7,6 +8,7 @@ import {
   TrashIcon,
 } from '@heroicons/react/24/outline';
 import Avatar from '../Avatar';
+import { Markdown } from './Markdown';
 
 interface IconBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -34,7 +36,7 @@ export const UserMessage = ({
 }) => {
   const [input, setInput] = useState(initialText);
   const [isEditing, setIsEditing] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
   const isCommenter = actor === 'commenter';
 
   useEffect(() => {
@@ -65,91 +67,77 @@ export const UserMessage = ({
     <div
       className={`
       flex 
-      justify-between 
-      ${
-        isCommenter ? 'bg-white bg-opacity-[2%]' : 'bg-white bg-opacity-5 ' //bg-yellow-200 bg-opacity-50
-      }
+      items-center justify-center text-white/70
+      ${isCommenter ? 'bg-white bg-opacity-[2%]' : 'bg-white bg-opacity-5 '}
       focus-within:ring-gray-700/70
       hover:bg-gray-700/20
       focus:text-gray-50
       ${isEditing ? 'ring-1 ring-gray-500/30' : ''}
-      grid-gap-2 
-      group
-      grid grid-cols-12 items-center
-      overflow-hidden
-      py-3
+      grid-gap-2 group overflow-hidden py-3
       `}
     >
-      <div className="col-span-2">
-        <div className="float-right">
+      <div className="flex w-full max-w-3xl items-start justify-start gap-2">
+        <div className="w-6">
           {!isCommenter ? (
             <Avatar actor="user" />
           ) : (
-            // <div className="h-[16px] w-[16px] rounded-full bg-teal-500" />
             <PaperClipIcon className="h-[16px] w-[16px] text-teal-500" />
           )}
         </div>
-      </div>
-
-      <input
-        ref={inputRef}
-        className={`
-        col-span-8 flex h-full w-full 
-        flex-col
-        bg-transparent
-        p-2
-        px-8
-        text-white/70
-        focus:outline-none
-      `}
-        value={input}
-        onClick={() => setIsEditing(true)}
-        onChange={(e) => {
-          setIsEditing(true);
-          setInput(e.target.value);
-        }}
-        onBlur={() => {
-          setInput(initialText);
-          setIsEditing(false);
-        }}
-        onFocus={() => setIsEditing(true)}
-      />
-
-      {isEditing ? (
-        <div className="m-auto mr-2 flex gap-2">
-          <div
-            className="rounded-md bg-gray-500/25 p-1.5 text-xs uppercase text-gray-100 hover:text-white"
-            onClick={submitRegenerate}
-          >
-            enter
-          </div>
-          <span className="rounded-md bg-gray-500/25 p-1.5 text-xs uppercase text-gray-100 hover:text-white">
-            esc
-          </span>
-        </div>
-      ) : (
-        <>
-          <div className="durtaion-200 flex items-center gap-2 opacity-0 group-hover:opacity-100">
-            <IconBtn
-              onClick={() => setIsEditing(true)}
-              className={`${iconBtnBaseStyle} hover:bg-teal-100/10`}
-            >
-              <PencilIcon className="h-4 w-4" />
-            </IconBtn>
-            <IconBtn className={`${iconBtnBaseStyle} hover:bg-red-800/60`} onClick={submitDelete}>
-              <TrashIcon className="h-4 w-4" />
-            </IconBtn>
-            {!isCommenter && (
-              <IconBtn
+        {isEditing ? (
+          <>
+            <TextareaAutosize
+              ref={inputRef}
+              className="h-full w-full resize-none rounded border-gray-800 bg-inherit p-2 text-white/70 focus:border focus:outline-none"
+              min-rows={4}
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              onBlur={() => {
+                setInput(initialText);
+                setIsEditing(false);
+              }}
+            />
+            <div className="m-auto mr-2 flex gap-2">
+              <div
+                className="rounded-md bg-gray-500/25 p-1.5 text-xs uppercase text-gray-100 hover:text-white"
                 onClick={submitRegenerate}
-                className={`${iconBtnBaseStyle} hover:bg-teal-800/60`}
               >
-                <PaperAirplaneIcon className="h-4 w-4" />
+                enter
+              </div>
+              <span className="rounded-md bg-gray-500/25 p-1.5 text-xs uppercase text-gray-100 hover:text-white">
+                esc
+              </span>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="-mt-1 w-full" onClick={() => setIsEditing(true)}>
+              <Markdown>{input}</Markdown>
+            </div>
+            <div className="durtaion-200 flex items-center gap-2 opacity-0 group-hover:opacity-100">
+              <IconBtn
+                onClick={() => setIsEditing(true)}
+                className={`${iconBtnBaseStyle} hover:bg-teal-100/10`}
+              >
+                <PencilIcon className="h-4 w-4" />
               </IconBtn>
-            )}
-          </div>
-        </>
-      )}
+              <IconBtn className={`${iconBtnBaseStyle} hover:bg-red-800/60`} onClick={submitDelete}>
+                <TrashIcon className="h-4 w-4" />
+              </IconBtn>
+              {!isCommenter && (
+                <IconBtn
+                  onClick={submitRegenerate}
+                  className={`${iconBtnBaseStyle} hover:bg-teal-800/60`}
+                >
+                  <PaperAirplaneIcon className="h-4 w-4" />
+                </IconBtn>
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
