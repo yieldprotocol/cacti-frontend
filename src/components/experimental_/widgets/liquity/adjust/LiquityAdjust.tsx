e54f3909fd074d53a626efea301d158e7189a019
marcomariscal <marco.a.mariscal@gmail.com>
2023-06-26 15:10:28 -0700

    fix: labels

--- src/components/experimental_/widgets/liquity/adjust/LiquityAdjust.tsx
+++ src/components/experimental_/widgets/liquity/adjust/LiquityAdjust.tsx
@@ -38,7 +38,7 @@ const LiquityAdjust = ({
   const repayLUSD = handleInput(repayAmount);
   const _withdrawCollateral = handleInput(withdrawCollateral);
   const _depositCollateral = handleInput(depositCollateral);
-  const isBorrow = !!borrowAmount || !!depositCollateral;
+  const isBorrow = !!borrowLUSD || !!_depositCollateral;
   const [label, setLabel] = useState<string>();
 
   const [sendParams, setSendParams] = useState<UnsignedTransaction>();
@@ -55,15 +55,29 @@ const LiquityAdjust = ({
       if (_depositCollateral) validParams.depositCollateral = _depositCollateral;
       if (_withdrawCollateral) validParams.withdrawCollateral = _withdrawCollateral;
 
+      // handle if for some reason there is a borrow and a repay input
+      if (borrowLUSD && repayLUSD) {
+        validParams.repayLUSD = 0; // TODO handle more gracefully to identify what user actually wants to do
+      }
+
+      // handle if for some reason there is a deposit and a withdraw input
+      if (_depositCollateral && _withdrawCollateral) {
+        validParams.withdrawCollateral = 0; // TODO handle more gracefully to identify what user actually wants to do
+      }
+
       const { rawPopulatedTransaction: params } = await liquity.populate.adjustTrove(validParams);
       setSendParams(params);
 
-      setLabel(
-        isBorrow
-          ? `Adjust Trove: Borrow ${borrowLUSD} ${'LUSD'} using ${_depositCollateral} ${'ETH'}`
-          : `Adjust Trove: Repay ${repayLUSD} ${'LUSD'} and withdraw ${_withdrawCollateral} ${'ETH'}`
-      );
+      // handle repay label
+      const labels: { [action: string]: string } = {};
+      if (repayLUSD) labels.repayLUSD = `Repay ${repayLUSD} ${'LUSD'}`;
+      if (_withdrawCollateral)
+        labels.withdrawCollateral = `Withdraw ${_withdrawCollateral} ${'ETH'}`;
+      if (borrowLUSD) labels.borrowLUSD = `Borrow ${borrowLUSD} ${'LUSD'}`;
+      if (_depositCollateral) labels.depositCollateral = `Deposit ${_depositCollateral} ${'ETH'}`;
+      const label = Object.values(labels).join(' and ');
 
+      setLabel(label);
       setSendParams(params);
     })();
   }, [_depositCollateral, _withdrawCollateral, borrowLUSD, isBorrow, repayLUSD, signer]);
