0ada2ff9810c637a0ab352cd9a83768e807c063f
Kah Keng Tay <kahkeng@gmail.com>
2023-06-27 16:14:57 -0700

    sidebar: add chat list queried from backend

--- src/components/experimental_/ChatList.tsx
+++ src/components/experimental_/ChatList.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 import { CheckCircleIcon, CheckIcon } from '@heroicons/react/24/outline';
+import { useQueryChats } from '@/api/queries';
 import ChatContext from '@/contexts/ChatContext';
 import { abbreviateHash, shortenAddress } from '@/utils';
 
@@ -9,41 +10,42 @@ export type ChatItem = {
 };
 
 const ChatItem = ({ id, selected }: ChatItem) => {
+  const onClick = selected
+    ? undefined
+    : () => {
+        const q = window.location.search;
+        const params = new URLSearchParams(q);
+        params.set('s', id);
+        window.location.assign('?' + params.toString());
+      };
   return (
     <div
-      className={`flex cursor-pointer flex-row items-center gap-2 rounded-sm py-2 ${
-        selected ? 'bg-white/5' : ''
+      className={`flex cursor-pointer flex-row items-center gap-2 rounded-sm py-2 text-white/70 ${
+        selected ? 'bg-white/5' : 'hover:bg-white/10 hover:text-white'
       } `}
+      onClick={onClick}
     >
       <div className="h-4 w-4 text-green-600"> {selected ? <CheckIcon /> : <div />}</div>
-      <div className="text-xs text-white/70 hover:text-white"> {abbreviateHash(id, 4)}</div>
+      <div className="text-xs"> {abbreviateHash(id, 4)}</div>
     </div>
   );
 };
 
 const ChatList = () => {
-  const chatList = [
-    { id: '9e6bd877-d76f-4379-95b3-7df36c9cc8d0' },
-    { id: 'e0cfadee-4baf-43e2-a589-f2f70ca87201' },
-    { id: '0892b8fb-81e2-44e4-92f4-11cb5b63e51d' },
-  ];
-  const [selectedId, setSelectedId] = useState<string>();
+  const { isSuccess, chats } = useQueryChats();
 
-  useEffect(() => {
-    const q = window.location.search;
-    const params = new URLSearchParams(q);
-    if (params.get('s')) {
-      setSelectedId(params.get('s') as string);
-    }
-  }, []);
+  const q = window.location.search;
+  const params = new URLSearchParams(q);
+  const selectedId = params.get('s') ? (params.get('s') as string) : '';
 
   return (
     <>
       <div className="pt-8 text-xs ">My Chats</div>
       <div className="py-4">
-        {chatList.map((chat: any) => {
-          return <ChatItem key={chat.id} id={chat.id} selected={selectedId === chat.id} />;
-        }, [])}
+        {isSuccess &&
+          chats?.sessions?.map((chat: any) => {
+            return <ChatItem key={chat.id} id={chat.id} selected={selectedId === chat.id} />;
+          }, [])}
       </div>
     </>
   );
