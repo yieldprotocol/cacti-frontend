f3cdd6d22c6f7d1fd3a1d30ec1b1b0ed05df45c1
marcomariscal <marco.a.mariscal@gmail.com>
2023-07-05 14:07:57 -0700

    fix: use Link

--- src/components/experimental_/ChatList.tsx
+++ src/components/experimental_/ChatList.tsx
@@ -1,9 +1,8 @@
-import { useContext, useEffect, useState } from 'react';
-import { CheckCircleIcon, CheckIcon } from '@heroicons/react/24/outline';
-import { useQueryChats } from '@/api/queries';
-import ChatContext from '@/contexts/ChatContext';
-import { abbreviateHash, shortenAddress } from '@/utils';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { CheckIcon } from '@heroicons/react/24/outline';
+import { useQueryChats } from '@/api/queries';
+import { abbreviateHash } from '@/utils';
 
 export type ChatItem = {
   id: string;
@@ -11,36 +10,31 @@ export type ChatItem = {
 };
 
 const ChatItem = ({ id, selected }: ChatItem) => {
-  const router = useRouter();
-
-  const onClick = selected
-    ? undefined
-    : () => router.push(id, undefined, { shallow: true })
-
   return (
-    <div
+    <Link
       className={`flex cursor-pointer flex-row items-center gap-2 rounded-sm py-2 text-white/70 ${
         selected ? 'bg-white/5' : 'hover:bg-white/10 hover:text-white'
       } `}
-      onClick={onClick}
+      href={`/${id}`}
     >
       <div className="h-4 w-4 text-green-600"> {selected ? <CheckIcon /> : <div />}</div>
       <div className="text-xs"> {abbreviateHash(id, 4)}</div>
-    </div>
+    </Link>
   );
 };
 
 const ChatList = () => {
-  const { isSuccess, chats } = useQueryChats();
+  const {
+    chats: { sessions },
+  } = useQueryChats();
   const router = useRouter();
   return (
     <>
       <div className="pt-8 text-xs ">My Chats</div>
       <div className="py-4">
-        {isSuccess &&
-          chats?.sessions?.map((chat: any) => {
-            return <ChatItem key={chat.id} id={chat.id} selected={ router.query.thread?.[0] === chat.id} />;
-          }, [])}
+        {sessions.map((chat) => (
+          <ChatItem key={chat.id} id={chat.id} selected={router.query.thread?.[0] === chat.id} />
+        ))}
       </div>
     </>
   );
