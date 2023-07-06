3f6c3e37f5b65a6f711febe4a32c996a45c02cdd
marcomariscal <marco.a.mariscal@gmail.com>
2023-07-06 14:25:33 -0700

    chore: remove unused

--- src/components/experimental_/ChatBox_.tsx
+++ src/components/experimental_/ChatBox_.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useQueryChats } from '@/api/queries';
 import { useChatContext } from '@/contexts/ChatContext';
