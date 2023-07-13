1ce9b7c622ce43c3f8b3eef2f7de47613ae40c18
Kah Keng Tay <kahkeng@gmail.com>
2023-07-13 14:12:56 -0700

    share: make forking push to the router

--- src/api/mutations.ts
+++ src/api/mutations.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from 'react-query';
 import { postCloneSession } from '@/api/posts';
 import { putShareSettings } from '@/api/puts';
+import { useRouter } from 'next/router';
 
 export const useMutationUpdateShareSettings = (sessionId: string) => {
   const mutationFn = ({ metadata }: { metadata: any }) => {
@@ -20,14 +21,15 @@ export const useMutationCloneSession = (sessionId: string) => {
   const mutationFn = ({ metadata }: { metadata: any }) => {
     return postCloneSession(sessionId, metadata);
   };
+  const router = useRouter();
+  const queryClient = useQueryClient();
 
   return useMutation(mutationFn, {
     onSuccess: (data, variables, context): void => {
       if (data) {
-        const q = window.location.search;
-        const params = new URLSearchParams(q);
-        params.set('s', data);
-        window.location.assign('?' + params.toString());
+        const newSessionId = data;
+        router.push(`/chat/${newSessionId}`);
+        queryClient.invalidateQueries({ queryKey: ['chats'] });
       }
     },
   });
