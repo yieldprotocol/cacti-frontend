d093dc90f153fd5bdadc0424941f63dc9bcfa53f
Kah Keng Tay <kahkeng@gmail.com>
2023-07-14 11:10:20 -0700

    Remove top level id page

--- src/pages/[id].tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const ChatBoxDynamic = dynamic(() => import('@/components/experimental_/ChatBox_'), {
-  ssr: false,
-});
-
-export const Chat = () => <ChatBoxDynamic />;
-
-export default Chat;
