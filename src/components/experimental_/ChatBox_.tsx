16c3be204f97a4565d90a1a7984d6cd1b79ccb35
marcomariscal <marco.a.mariscal@gmail.com>
2023-07-20 10:20:22 -0700

    fix: remove blur

--- src/components/experimental_/ChatBox_.tsx
+++ src/components/experimental_/ChatBox_.tsx
@@ -23,7 +23,7 @@ const ChatBox = () => {
       </div>
 
       {/* Chat input */}
-      <div className="fixed inset-x-0 bottom-0 flex w-full items-center bg-white/[0.01] px-2 py-4 backdrop-blur-xl lg:py-6">
+      <div className="fixed inset-x-0 bottom-0 flex w-full items-center bg-white/[0.01] px-2 py-4 lg:py-6">
         <div className="hidden lg:block lg:w-[15rem]" />
         <MessageInput />
       </div>
