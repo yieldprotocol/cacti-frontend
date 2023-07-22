ed417f26aae33f9e5308d2b2ef058caa60e4e144
Kah Keng Tay <kahkeng@gmail.com>
2023-07-22 00:34:49 -0700

    Move API calls to match REST in backend

---
+++ src/api/shares/mutations.ts
@@ -0,0 +1,50 @@
+import { useMutation, useQueryClient } from 'react-query';
+import { useRouter } from 'next/router';
+import { deleteSharedSession, postCreateSharedSession, putSharedSession } from '@/api/shares/calls';
+
+export const useMutationUpdateSharedSession = (sharedSessionId: string) => {
+  const mutationFn = ({ metadata }: { metadata: any }) => {
+    return putSharedSession(sharedSessionId, metadata);
+  };
+
+  const queryClient = useQueryClient();
+
+  return useMutation(mutationFn, {
+    onSuccess: (data, variables, context): void => {
+      queryClient.invalidateQueries({ queryKey: ['sharedSession', sharedSessionId] });
+    },
+  });
+};
+
+export const useMutationDeleteSharedSession = (sharedSessionId: string) => {
+  const mutationFn = () => {
+    return deleteSharedSession(sharedSessionId);
+  };
+
+  const queryClient = useQueryClient();
+
+  return useMutation(mutationFn, {
+    onSuccess: (data, variables, context): void => {
+      queryClient.invalidateQueries({ queryKey: ['shares'] });
+      queryClient.invalidateQueries({ queryKey: ['sharedSession', sharedSessionId] });
+    },
+  });
+};
+
+export const useMutationCreateSharedSession = (sessionId: string) => {
+  const mutationFn = ({ metadata }: { metadata: any }) => {
+    return postCreateSharedSession(sessionId, metadata);
+  };
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  return useMutation(mutationFn, {
+    onSuccess: (data, variables, context): void => {
+      if (data) {
+        const newSessionId = data;
+        router.push(`/shares/${newSessionId}`);
+        queryClient.invalidateQueries({ queryKey: ['shares'] });
+      }
+    },
+  });
+};
