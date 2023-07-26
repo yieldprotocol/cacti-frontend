4b9b148621c4e7669d03ea78a559b1924701e6aa
marcomariscal <marco.a.mariscal@gmail.com>
2023-07-26 10:31:39 -0700

    fix: hanlde undefined sessionId

--- src/api/chats/queries.ts
+++ src/api/chats/queries.ts
@@ -22,12 +22,14 @@ export interface ChatSettings {
   name?: string;
   canEdit?: boolean;
 }
-export const useQueryChatSettings = (sessionId: string) => {
+export const useQueryChatSettings = (sessionId: string | undefined) => {
   const { data: sessionData } = useSession();
   const userId = sessionData?.user?.name || '';
   const { data: settings, ...rest } = useQuery<ChatSettings>(
     ['chatSettings', sessionId, userId],
-    async () => getChatSettings(sessionId)
+    async () => {
+      if (sessionId) return getChatSettings(sessionId);
+    }
   );
   return { settings, ...rest };
 };
