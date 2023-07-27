111d03f7f29d8e78732e9355a5b0752718d1aa79
marcomariscal <marco.a.mariscal@gmail.com>
2023-07-27 08:35:33 -0700

    feat: no need to revalidate on window focus for now

--- src/api/chats/queries.ts
+++ src/api/chats/queries.ts
@@ -13,7 +13,11 @@ export interface Chats {
 export const useQueryChats = () => {
   const { data: sessionData } = useSession();
   const userId = sessionData?.user?.name || '';
-  const { data: chats, ...rest } = useQuery<Chats>(['chats', userId], async () => getChatsList());
+  const { data: chats, ...rest } = useQuery<Chats>({
+    queryKey: ['chats', userId],
+    queryFn: async () => getChatsList(),
+    refetchOnWindowFocus: false,
+  });
   return { chats, ...rest };
 };
 
