b17b9be078a9eba199ffe78b17558eb1bdcf5bc9
marcomariscal <marco.a.mariscal@gmail.com>
2023-07-27 08:04:37 -0700

    fix: className at the end to apply style override

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -44,7 +44,7 @@ export const findProjectByName = (name: string): Project => {
 
 export const Spinner = ({ className }: { className?: string }) => (
   <svg
-    className={`${className ?? ''} animate-spin text-gray-100`}
+    className={`h-4 w-4 animate-spin text-gray-100 ${className ?? ''}`}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
