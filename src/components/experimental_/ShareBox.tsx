0c1a132a35bd36fe2c03838635f6544f9e1bd717
brucedonovan <bruce.donovan@gmail.com>
2023-07-27 14:11:11 +0100

    continue chatting button

--- src/components/experimental_/ShareBox.tsx
+++ src/components/experimental_/ShareBox.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { useMutationChatImportSession } from '@/api/chats/mutations';
 import { useQuerySharedSession, useQueryShares } from '@/api/shares/queries';
 import { Message, useChatContext } from '@/contexts/ChatContext';
 import { Spinner } from '@/utils';
@@ -9,11 +10,12 @@ const ShareBox = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  const {showDebugMessages} = useChatContext();
+  const { showDebugMessages } = useChatContext();
 
   const { isLoading, isSuccess, settings } = useQuerySharedSession(id as string);
-
-  console.log(settings?.messages);
+  const { mutate: continueChat, isLoading: isCreatingChat } = useMutationChatImportSession(
+    id as string
+  );
 
   const messageContentComponent = true ? (
     <div className="h-full w-full pt-8">
@@ -34,13 +36,31 @@ const ShareBox = () => {
   );
 
   return (
-    <div className="relative flex h-full w-full flex-col overflow-auto">
-      <div className="flex h-full w-full items-center justify-center overflow-auto pt-5">
-        {isLoading || !isSuccess ? <Spinner  /> : messageContentComponent}
+    <div className="relative flex h-full w-full flex-col overflow-auto ">
+      <div
+        className={`${
+          isCreatingChat && 'blur'
+        }  flex h-full w-full items-center justify-center overflow-auto pt-5`}
+      >
+        {isLoading || !isSuccess ? <Spinner /> : messageContentComponent}
       </div>
-      {/* convert to chat button */}
-      <div className="sticky top-[100vh] flex w-full items-center justify-center justify-items-center bg-gray-secondary px-2 py-4 lg:py-6">
-        Convert to chat
+      <div
+        className={` sticky top-[100vh] flex w-full items-center justify-center justify-items-center bg-gray-secondary px-2 py-4 lg:py-6`}
+      >
+        <button
+          onClick={() => continueChat({ metadata: {} })}
+          className="rounded-lg bg-green-primary px-2 py-1 hover:bg-green-primary/80 "
+        >
+          <div className="flex items-center gap-2 p-2 text-gray-300">
+            {isCreatingChat ? (
+              <>
+                <Spinner className="text-gray-300" /> <div>Converting to a new chat</div>
+              </>
+            ) : (
+              <div>Continue chatting</div>
+            )}
+          </div>
+        </button>
       </div>
     </div>
   );
