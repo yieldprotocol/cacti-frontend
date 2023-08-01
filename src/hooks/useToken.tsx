a92a23dc4c8a2d1ad39418766d7c771f0974f7ff
marcomariscal <marco.a.mariscal@gmail.com>
2023-08-01 13:33:26 -0700

    fix: add name for eth

--- src/hooks/useToken.tsx
+++ src/hooks/useToken.tsx
@@ -23,6 +23,7 @@ const useToken = (tokenSymbol?: string, tokenAddress?: Address) => {
           decimals: 18,
           logoURI:
             'https://storage.googleapis.com/zapper-fi-assets/tokens/ethereum/0x0000000000000000000000000000000000000000.png',
+          name: 'Ether',
         } as Token;
       if (tokenSymbol) return findTokenBySymbol(tokenSymbol, chainId) as Token;
       if (tokenAddress) return findTokenByAddress(tokenAddress, chainId) as Token;
