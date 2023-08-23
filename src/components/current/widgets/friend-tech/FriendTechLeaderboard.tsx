b26aeaea85934691af72cba15232ce2ce2936ca5
marcomariscal <marco.a.mariscal@gmail.com>
2023-08-23 14:18:22 -0700

    feat: friend tech leaderboard prelim implementation

---
+++ src/components/current/widgets/friend-tech/FriendTechLeaderboard.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import { HeaderResponse, SingleLineResponse } from '@/components/cactiComponents';
+import { ResponseGrid, ResponseTitle } from '@/components/cactiComponents/helpers/layout';
+import useLeaderboardData, { LeaderboardUserItem } from './useLeaderboardData';
+
+const FriendTechLeaderboard = () => {
+  const { data } = useLeaderboardData();
+  // get the first 10 users from the leaderboard
+  const sliced = data?.slice(0, 10);
+
+  return (
+    <>
+      <HeaderResponse text="Friend.Tech Leaderboard" />
+      <ResponseGrid className="grid gap-1">
+        {sliced?.map((user) => (
+          <LeaderboardItem key={user.address} user={user} />
+        ))}
+      </ResponseGrid>
+    </>
+  );
+};
+
+const LeaderboardItem = ({ user }: { user: LeaderboardUserItem }) => {
+  return (
+    <SingleLineResponse className="flex justify-between">
+      <ResponseTitle>@{user.username}</ResponseTitle>
+      <ResponseTitle>{user.price} ETH</ResponseTitle>
+    </SingleLineResponse>
+  );
+};
+
+export default FriendTechLeaderboard;
