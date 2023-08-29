ec59d55b901fb2f8c78bc6043d641509cb8a8910
marcomariscal <marco.a.mariscal@gmail.com>
2023-08-29 13:08:47 -0700

    feat: handle custom inputs

--- src/components/current/widgets/tx-replay/TransactionReplay.tsx
+++ src/components/current/widgets/tx-replay/TransactionReplay.tsx
@@ -1,38 +1,45 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
 import axios from 'axios';
-import { BigNumber, ethers } from 'ethers';
-import { decodeFunctionData, isAddress, maxUint256 } from 'viem';
-import { Address, useTransaction } from 'wagmi';
-import {
-  ActionResponse,
-  HeaderResponse,
-  ListResponse,
-  SingleLineResponse,
-} from '@/components/cactiComponents';
+import { BigNumber, UnsignedTransaction, ethers } from 'ethers';
+import { decodeFunctionData } from 'viem';
+import { Address, usePrepareContractWrite, usePrepareSendTransaction, useTransaction } from 'wagmi';
+import { ActionResponse, HeaderResponse, SingleLineResponse } from '@/components/cactiComponents';
+import { TxBasicParams } from '@/components/cactiComponents/hooks/useSubmitTx';
 import SkeletonWrap from '@/components/shared/SkeletonWrap';
 
 interface TransactionReplayProps {
   txHash: Address;
 }
 
+const Input = ({ name, value, onChange }: { name: string; value: string; onChange: any }) => (
+  <input
+    name={name}
+    className="mt-2 w-full rounded-md border border-gray-700 bg-gray-primary p-2 hover:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-500"
+    value={value}
+    placeholder={value}
+    onChange={onChange}
+  />
+);
+
 const TransactionReplay = ({ txHash }: TransactionReplayProps) => {
   const explorerUrl = `https://etherscan.io/tx/${txHash}`;
 
+  // handles the state for the original decoded tx, as well as any arg or value input changes
   const [decoded, setDecoded] = useState<{
     functionName: string;
     args: {
       name: string; // name of the argument
-      value: string | BigNumber; // value of the argument
-      displayValue: string; // formatted value based on if the argument is an address or value
+      value: string; // value of the argument
+      type: string; // type of the argument
     }[];
-    value?: { value: BigNumber; displayValue: string };
+    value: string;
   }>();
 
   // decode transaction function with parameters
   // set parameter values to the original transaction
   // set up form from transaction
-  const { data } = useTransaction({ hash: txHash });
+  const { data, isLoading } = useTransaction({ hash: txHash });
 
   // get abi from etherscan using tx hash
   const getAbi = async (contractAddress: string | undefined) => {
@@ -53,12 +60,26 @@ const TransactionReplay = ({ txHash }: TransactionReplayProps) => {
   const getArgsTypes = ({ abi, functionName }: { abi: any[]; functionName: string }) =>
     abi.find((f) => f.name === functionName).inputs;
 
-  // check if the argument value is an address or value
-  const handleDisplayValue = (value: string | bigint | BigNumber | undefined) => {
-    if (!value) return 'could not parse value';
-    if (value === maxUint256) return 'maxUint256';
-    const _value = value.toString();
-    return isAddress(_value) ? _value : _value.replace('n', '');
+  // handle arg changes
+  const handleArgChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    // handle changing the value param
+    if (name === 'value') {
+      setDecoded((d) => d && { ...d, value });
+      return;
+    }
+
+    // handle changing the args
+    setDecoded((d) => {
+      if (!d) return;
+
+      const newArgs = [...d.args];
+      const argIndex = newArgs.findIndex((arg) => arg.name === name);
+      if (argIndex !== -1) newArgs[argIndex] = { ...newArgs[argIndex], value: value };
+
+      return { ...d, args: newArgs };
+    });
   };
 
   // handle a transfer transaction
@@ -68,17 +89,13 @@ const TransactionReplay = ({ txHash }: TransactionReplayProps) => {
       {
         name: 'to',
         value: data?.to || 'No to address detected',
-        displayValue: handleDisplayValue(data?.to),
-      },
-      {
-        name: 'value',
-        value: data?.value || ethers.constants.Zero,
-        displayValue: handleDisplayValue(data?.value),
+        type: 'address',
       },
     ];
     setDecoded({
       functionName,
       args,
+      value: data?.value.toString() || '0',
     });
   }, [data?.to, data?.value]);
 
@@ -96,13 +113,13 @@ const TransactionReplay = ({ txHash }: TransactionReplayProps) => {
     const args = _args.map((_, i) => ({
       name: types[i].name as string,
       value: _args[i],
-      displayValue: handleDisplayValue(_args[i]),
+      type: types[i].type as string,
     }));
 
     setDecoded({
       functionName,
       args,
-      value: { value: data.value, displayValue: data.value.toString() },
+      value: data?.value.toString() || '0',
     });
   }, [abi, data]);
 
@@ -120,23 +137,61 @@ const TransactionReplay = ({ txHash }: TransactionReplayProps) => {
     decodeTransaction();
   }, [decodeTransaction]);
 
+  // handle transfer transaction
+  const sendParams = useMemo<UnsignedTransaction | undefined>(
+    () =>
+      decoded?.functionName === 'transfer ETH'
+        ? { to: decoded.args[0].value, value: BigNumber.from(decoded?.value) }
+        : undefined,
+    [decoded?.functionName, decoded?.args, decoded?.value]
+  );
+
+  // handle contract call transaction
+  const txParams = useMemo<TxBasicParams | undefined>(
+    () =>
+      decoded?.functionName !== 'transfer ETH'
+        ? {
+            abi,
+            address: data?.to as Address | undefined,
+            functionName: decoded?.functionName,
+            args: decoded?.args.map((arg) =>
+              arg.type === 'address' ? arg.value : BigNumber.from(arg.value)
+            ),
+            value: decoded?.value,
+          }
+        : undefined,
+    [abi, data?.to, decoded?.args, decoded?.functionName, decoded?.value]
+  );
+
   return (
     <>
-      {!data && <div>no data found for this transaction</div>}
+      {!data && !isLoading && <div>no data found for this transaction</div>}
       {!decoded ? (
         <SkeletonWrap />
       ) : (
         <>
           <HeaderResponse text={`${decoded.functionName}`} altUrl={explorerUrl} />
-          <ListResponse data={decoded.args.map((a) => [a.name, a.displayValue])} />
-          {decoded.value?.value.gt(ethers.constants.Zero) && (
-            <SingleLineResponse value={`Value: ${decoded.value.displayValue}`} />
-          )}
-          <ActionResponse
-            txParams={undefined}
-            approvalParams={undefined}
-            sendParams={{ to: data?.to, data: data?.data, value: data?.value }}
-          />
+          <SingleLineResponse className="flex items-center justify-center p-2">
+            <form className="w-full p-2">
+              {decoded.args.map((arg, i) => (
+                <div className="mb-4" key={i}>
+                  <label htmlFor={arg.name} className="block font-medium text-gray-400">
+                    {arg.name}
+                  </label>
+                  <Input name={arg.name} value={arg.value} onChange={handleArgChange} />
+                </div>
+              ))}
+              {BigNumber.from(decoded.value)?.gt(ethers.constants.Zero) && (
+                <div className="mb-4">
+                  <label htmlFor={'value'} className="block font-medium text-gray-400">
+                    {'value'}
+                  </label>
+                  <Input name={'value'} value={decoded.value} onChange={handleArgChange} />
+                </div>
+              )}
+            </form>
+          </SingleLineResponse>
+          <ActionResponse txParams={txParams} sendParams={sendParams} approvalParams={undefined} />
         </>
       )}
     </>
