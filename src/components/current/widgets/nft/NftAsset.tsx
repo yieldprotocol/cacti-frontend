d4192d6527a2237503f53fff20196121c287b1af
brucedonovan <bruce.donovan@gmail.com>
2023-09-04 10:17:52 +0100

    wip

--- src/components/current/widgets/nft/NftAsset.tsx
+++ src/components/current/widgets/nft/NftAsset.tsx
@@ -3,6 +3,7 @@ import { Address } from 'wagmi';
 import { ImageResponse } from '@/components/cactiComponents';
 import { ImageVariant } from '@/components/cactiComponents/ImageResponse';
 import { InlineChip } from '@/components/cactiComponents/InlineChip';
+import { getBackendApiUrl } from '@/utils/backend';
 
 export interface NftAssetProps {
   network: Network;
@@ -41,9 +42,11 @@ export const NftAsset = ({
   attributes
 }: NftAssetProps) => {
 
+  const backendUrl = getBackendApiUrl();
+  
   return (
     <ImageResponse
-      image={previewImageUrl}
+      image={`${backendUrl}/center_image/${network}/${address}/${tokenId}/small`}
       imageTags={
         variant === ImageVariant.SHOWCASE
           ? [`Token Id: ${tokenId}`, `${network.replace('-mainnet', '')}`]
