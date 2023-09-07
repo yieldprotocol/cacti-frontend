eed6065bfa4d73486d35cbbf4122d6bb85ffef50
brucedonovan <bruce.donovan@gmail.com>
2023-09-07 11:18:14 +0100

    fix build

--- src/components/devTools/MintButton.tsx
+++ src/components/devTools/MintButton.tsx
@@ -11,7 +11,7 @@ export const MintButton = () => {
   const [isVisible, setVisible] = useState(false);
   const { chain } = useNetwork();
   const { refetch } = useBalance();
-  const provider = useProvider() as JsonRpcProvider;
+  const provider = useProvider();
 
   useEffect(() => {
     if (!address || chain?.id != 1) setVisible(false);
@@ -24,6 +24,7 @@ export const MintButton = () => {
       ethers.utils.parseEther('10').toHexString(), // hex encoded wei amount
     ];
     setLoading(true);
+    //@ts-ignore
     await provider.send('tenderly_addBalance', params);
     await refetch();
     setLoading(false);
