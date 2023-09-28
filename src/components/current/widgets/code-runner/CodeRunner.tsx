21df2ce9323d8e67e6cde9c733113a6a293863d1
marcomariscal <marco.a.mariscal@gmail.com>
2023-09-28 13:08:46 -0700

    feat: trying to execute arbitrary code

--- src/components/current/widgets/code-runner/CodeRunner.tsx
+++ src/components/current/widgets/code-runner/CodeRunner.tsx
@@ -1,24 +1,30 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula as style } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useProvider } from 'wagmi';
 import { Button } from '@/components/shared/Button';
+import useSigner from '@/hooks/useSigner';
 
 interface CodeRunnerProps {
   codeString: string;
 }
 
 const CodeRunner = ({ codeString }: CodeRunnerProps) => {
+  const provider = useProvider();
+  const signer = useSigner();
+
   const executeCode = async () => {
     try {
       // handle async funcs
-      const wrappedCode = `(async () => { ${codeString} })()`;
+      const wrappedCode = `(async (provider, signer) => { return ${codeString}; })(provider, signer)`;
 
       // Caution: using eval is generally not safe.
       // TODO make sure the codeString can be trusted.
       const result = await eval(wrappedCode);
+      console.log('🦄 ~ file: CodeRunner.tsx:23 ~ executeCode ~ result:', result);
 
       if (typeof result === 'function') {
-        const res = result();
-        console.log('res', res);
+        const res = await result(provider, signer);
+        console.log('🦄 ~ file: CodeRunner.tsx:26 ~ executeCode ~ res:', res);
       }
     } catch (e) {
       console.error('An error occurred:', e);
@@ -30,7 +36,7 @@ const CodeRunner = ({ codeString }: CodeRunnerProps) => {
       <SyntaxHighlighter language="javascript" style={style}>
         {codeString.trim()}
       </SyntaxHighlighter>
-      <Button action={executeCode}>Submit</Button>
+      <Button onClick={() => executeCode}>Submit</Button>
     </div>
   );
 };
--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,21 +1,21 @@
-export const Button = (props: any) => {
-  return (
-    <button
-      {...props}
-      className={`
-        inline-block 
-        w-full 
-        cursor-pointer 
-        justify-center 
-        rounded-xl
-         bg-gray-600 px-6 py-2.5 text-center text-sm font-bold leading-tight text-white shadow-md 
-         transition duration-150 
-         ease-in-out 
-         hover:bg-gray-700 hover:shadow-lg 
-         focus:shadow-lg focus:outline-none focus:ring-0 
-         active:shadow-lg 
-         disabled:cursor-not-allowed disabled:bg-gray-500 
-         ${props.className}`}
-    />
-  );
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+export const Button = ({ className, ...props }: ButtonProps) => {
+  const baseClasses = `
+    h-10 inline-flex w-full 
+    cursor-pointer items-center justify-center 
+    rounded-md border border-white/10 
+    bg-green-primary p-2 px-6 py-2.5 
+    text-center text-sm font-bold leading-tight text-white/70 
+    shadow-sm transition duration-200 ease-in-out 
+    hover:bg-green-primary/70 
+    focus:outline-none focus:ring-0 
+    disabled:cursor-not-allowed disabled:bg-gray-500
+  `;
+
+  const allClasses = `${baseClasses} ${className}`;
+
+  return <button {...props} className={allClasses} />;
 };
